1296d78b51f22fc6f44858eb79d27ea0c75769bd
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-04 15:57:11 +0500

    98. Validate Binary Search Tree
    https://leetcode.com/problems/validate-binary-search-tree/

---
+++ Tree/98. Validate Binary Search Tree/validate-binary-search-tree.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        func helper(_ node: TreeNode?, _ min: Int?, _ max: Int?) -> Bool {
+            guard let node = node else { return true }
+            if let min = min, node.val <= min { return false }
+            if let max = max, node.val >= max { return false }
+            return helper(node.left, min, node.val) && helper(node.right, node.val, max)
+        }
+        return helper(root, nil, nil)
+    }    
+}
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
