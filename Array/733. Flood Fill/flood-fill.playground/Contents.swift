e2644d60e2668ee0405b7ba000ee59ad37e58888
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 08:02:40 +0500

    Update Contents.swift

--- Array/733. Flood Fill/flood-fill.playground/Contents.swift
+++ Array/733. Flood Fill/flood-fill.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
+// 733. Flood Fill
 // https://leetcode.com/problems/flood-fill
-// Discuss: https://vk.cc/c3Iyqw
 
 class Solution {
     func floodFill(_ image: [[Int]], _ sr: Int, _ sc: Int, _ newColor: Int) -> [[Int]] {
@@ -9,7 +9,7 @@ class Solution {
         dfs(&img, sr, sc, img[sr][sc], newColor)
         return img
     }
-    fileprivate func dfs(_ img: inout [[Int]], _ r: Int, _ c: Int, _ tc: Int, _ nc: Int) {
+    private func dfs(_ img: inout [[Int]], _ r: Int, _ c: Int, _ tc: Int, _ nc: Int) {
         if r < 0 || r >= img.count || c < 0 || c >= (img.first?.count ?? 0) { return }
         if img[r][c] != tc || img[r][c] == nc { return }
         img[r][c] = nc
@@ -22,12 +22,12 @@ import XCTest
 // Executed 2 tests, with 0 failures (0 unexpected) in 0.049 (0.050) seconds
 
 class Tests: XCTestCase {
-    let s = Solution()
-    func test1() {
+    private let s = Solution()
+    func test0() {
         let res = s.floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2)
         XCTAssertEqual(res, [[2,2,2],[2,2,0],[2,0,1]])
     }
-    func test2() {
+    func test1() {
         let res = s.floodFill([[0,0,0],[0,0,0]], 0, 0, 2)
         XCTAssertEqual(res, [[2,2,2],[2,2,2]])
     }
