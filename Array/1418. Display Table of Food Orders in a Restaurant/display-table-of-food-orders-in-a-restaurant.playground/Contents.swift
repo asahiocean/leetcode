e7a5458628397d7b525481600666946e1623c825
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 07:04:47 +0500

    Update Contents.swift

--- Array/1418. Display Table of Food Orders in a Restaurant/display-table-of-food-orders-in-a-restaurant.playground/Contents.swift
+++ Array/1418. Display Table of Food Orders in a Restaurant/display-table-of-food-orders-in-a-restaurant.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
+// 1418. Display Table of Food Orders in a Restaurant
 // https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
-// Discuss: https://vk.cc/c3PA6R
 
 class Solution {
     func displayTable(_ orders: [[String]]) -> [[String]] {
