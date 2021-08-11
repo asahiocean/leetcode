2ba4ac2e37b0ed339cc64feba4503e12b6d960e7
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 07:49:18 +0500

    Update Contents.swift

--- Hash Table/387. First Unique Character in a String/first-unique-character-in-a-string.playground/Contents.swift
+++ Hash Table/387. First Unique Character in a String/first-unique-character-in-a-string.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
+// 387. First Unique Character in a String
 // https://leetcode.com/problems/first-unique-character-in-a-string/
-// Discuss: https://vk.cc/c3KIil
 
 class Solution {
     func firstUniqChar(_ s: String) -> Int {
