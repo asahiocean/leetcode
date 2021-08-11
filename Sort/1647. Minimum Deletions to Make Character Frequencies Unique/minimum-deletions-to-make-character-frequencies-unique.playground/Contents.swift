16eab78bfef5a0ecbfb9bd424c1ac4d5fd481f57
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 20:24:24 +0500

    1647. Minimum Deletions to Make Character Frequencies Unique
    https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/

--- Sort/1647. Minimum Deletions to Make Character Frequencies Unique/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
+++ Sort/1647. Minimum Deletions to Make Character Frequencies Unique/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
@@ -1,14 +1,7 @@
 import Foundation
 
+// 1647. Minimum Deletions to Make Character Frequencies Unique
 // https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
-
-/*
-A string s is called good if there are no two different characters in s that have the same frequency.
-
-Given a string s, return the minimum number of characters you need to delete to make s good.
-
-The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
-*/
 
 class Solution {
     func minDeletions(_ s: String) -> Int {
@@ -32,11 +25,26 @@ class Solution {
     }
 }
 
-func tests() {
-    let s = Solution()
-    assert(s.minDeletions("aab") == 0) // good
-    assert(s.minDeletions("aaabbbcc") == 2) // good
-    assert(s.minDeletions("ceabaacb") == 2) // good
+// MARK: - Test Cases -
+
+import XCTest
+
+// Executed 3 tests, with 0 failures (0 unexpected) in 0.031 (0.033) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    func test0() {
+        let res = s.minDeletions("aab")
+        XCTAssertEqual(res, 0)
+    }
+    func test1() {
+        let res = s.minDeletions("aaabbbcc")
+        XCTAssertEqual(res, 2)
+    }
+    func test2() {
+        let res = s.minDeletions("ceabaacb")
+        XCTAssertEqual(res, 2)
+    }
 }
 
-tests()
+Tests.defaultTestSuite.run()
