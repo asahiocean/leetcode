e36ce0e15e07ff1c005be55588575cd691cc06d8
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 19:57:54 +0500

    258. Add Digits
    https://leetcode.com/problems/add-digits/

---
+++ Math/258. Add Digits/add-digits.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// 258. Add Digits
+// https://leetcode.com/problems/add-digits/
+
+class Solution {
+    func addDigits(_ num: Int) -> Int {
+        return num < 10 ? num : (num % 9 == 0 ? 9 : num % 9)
+    }
+}
