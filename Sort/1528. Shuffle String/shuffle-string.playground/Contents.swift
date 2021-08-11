f14eb47c13bb6f86165f6a5541d2b71e904aa69c
ASAHI OCEAN <asahiocean@icloud.com>
2021-08-11 20:29:29 +0500

    1528. Shuffle String
    https://leetcode.com/problems/shuffle-string/

--- Sort/1528. Shuffle String/shuffle-string.playground/Contents.swift
+++ Sort/1528. Shuffle String/shuffle-string.playground/Contents.swift
@@ -1,12 +1,17 @@
 import Foundation
 
+// 1528. Shuffle String
+// https://leetcode.com/problems/shuffle-string/
+
 class Solution {
     func restoreString(_ s: String, _ indices: [Int]) -> String {
-        var res = Array<Character>(repeating: "a", count: indices.count)
+        var res = [Character](repeating: "a", count: indices.count)
         for i in 0 ..< indices.count { res[indices[i]] = Array(s)[i] }
         return String(res)
     }
 }
+
+// MARK: - Test Cases -
 
 import XCTest
 
@@ -14,20 +19,20 @@ import XCTest
 
 class Tests: XCTestCase {
     private let s = Solution()
-    func testExample1() {
-        XCTAssertEqual(s.restoreString("codeleet", [4,5,6,7,0,2,1,3]), "leetcode") // success
+    func test0() {
+        XCTAssertEqual(s.restoreString("codeleet", [4,5,6,7,0,2,1,3]), "leetcode")
     }
-    func testExample2() {
-        XCTAssertEqual(s.restoreString("abc", [0,1,2]), "abc") // success
+    func test1() {
+        XCTAssertEqual(s.restoreString("abc", [0,1,2]), "abc")
     }
-    func testExample3() {
-        XCTAssertEqual(s.restoreString("aiohn", [3,1,4,2,0]), "nihao") // success
+    func test2() {
+        XCTAssertEqual(s.restoreString("aiohn", [3,1,4,2,0]), "nihao")
     }
-    func testExample4() {
-        XCTAssertEqual(s.restoreString("aaiougrt", [4,0,2,6,7,3,1,5]), "arigatou") // success
+    func test3() {
+        XCTAssertEqual(s.restoreString("aaiougrt", [4,0,2,6,7,3,1,5]), "arigatou")
     }
-    func testExample5() {
-        XCTAssertEqual(s.restoreString("art", [1,0,2]), "rat") // success
+    func test4() {
+        XCTAssertEqual(s.restoreString("art", [1,0,2]), "rat")
     }
 }
 
