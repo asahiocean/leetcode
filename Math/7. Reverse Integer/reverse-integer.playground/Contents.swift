8c667ff0ff604d64f80319bd89b5a5d9220efe5d
asahiocean <61920574+asahiocean@users.noreply.github.com>
2021-11-20 17:10:26 +0500

    7. Reverse Integer

---
+++ Math/7. Reverse Integer/reverse-integer.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// 7. Reverse Integer
+// https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var r = 0, x = x
+        while x != 0 {
+            r = r * 10
+            r = r + x % 10
+            x /= 10
+        }
+        if r < Int32.min || r > Int32.max { return 0 }
+        return r
+    }
+}
