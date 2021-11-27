5ab1541a0a1f49508bfd6b76107b6ba63a7e6094
asahiocean <61920574+asahiocean@users.noreply.github.com>
2021-11-27 18:17:00 +0500

    Update Contents.swift

--- Math/7. Reverse Integer/reverse-integer.playground/Contents.swift
+++ Math/7. Reverse Integer/reverse-integer.playground/Contents.swift
@@ -11,7 +11,36 @@ class Solution {
             r = r + x % 10
             x /= 10
         }
-        if r < Int32.min || r > Int32.max { return 0 }
-        return r
+        return r < Int32.min || r > Int32.max ? 0 : r
     }
 }
+
+// MARK: - Test Cases -
+
+// Result: Executed 4 tests, with 0 failures (0 unexpected) in 0.009 (0.011) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let s = Solution()
+    
+    func test0() {
+        let value = s.reverse(123)
+        XCTAssertEqual(value, 321)
+    }
+    func test1() {
+        let value = s.reverse(-123)
+        XCTAssertEqual(value, -321)
+    }
+    func test2() {
+        let value = s.reverse(120)
+        XCTAssertEqual(value, 21)
+    }
+    func test3() {
+        let value = s.reverse(0)
+        XCTAssertEqual(value, 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
