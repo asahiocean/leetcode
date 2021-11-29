ecb5895446103528bd3d4040e5e2f9604154b42e
asahiocean <asahiocean@icloud.com>
2021-11-29 23:53:28 +0500

    Update branch

---
+++ Array/1. Two Sum/two-sum.playground/Contents.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int:Int]()
+        for (i, num) in nums.enumerated() {
+            if let last = dict[target - num] {
+                return [last, i]
+            }
+            dict[num] = i
+        }
+        return []
+    }
+}
+
+// MARK: - Tests -
+
+import XCTest
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.022 (0.024) seconds
+
+class Tests: XCTestCase {
+    
+    private let s = Solution()
+    
+    func test0() {
+        let res = s.twoSum([2,7,11,15], 9)
+        XCTAssertEqual(res, [0,1])
+    }
+    func test1() {
+        let res = s.twoSum([3,2,4], 6)
+        XCTAssertEqual(res, [1,2])
+    }
+    func test2() {
+        let res = s.twoSum([3,3], 6)
+        XCTAssertEqual(res, [0,1])
+    }
+}
+
+Tests.defaultTestSuite.run()
