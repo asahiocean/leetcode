57d60bd91a0c8ba45764da15f8dd2ce5bba6edb1
asahiocean <asahiocean@icloud.com>
2021-12-24 09:53:57 +0500

    Refactoring: 19. Remove Nth Node From End of List
    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

--- 1-100/19. Remove Nth Node From End of List/remove-nth-node-from-end-of-list.playground/Contents.swift
+++ 1-100/19. Remove Nth Node From End of List/remove-nth-node-from-end-of-list.playground/Contents.swift
@@ -16,11 +16,13 @@ class Solution {
             post = next
         }
         
-        while post!.next != nil {
-            prev = prev!.next
-            post = post!.next
+        while let postNext = post?.next, let prevNext = prev?.next {
+            prev = prevNext
+            post = postNext
         }
+        
         prev!.next = prev!.next!.next
+        
         return node.next
     }
 }
@@ -31,4 +33,50 @@ public class ListNode {
     public init() { self.val = 0; self.next = nil; }
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    // An additional initializer that can be used to implement nodes from an array
+    public init?(_ array: [Int]) {
+        guard !array.isEmpty else { return nil }
+        self.val = array[0]
+        var prev: ListNode = self
+        for i in 1..<array.count {
+            let new = ListNode(array[i])
+            prev.next = new
+            prev = new
+        }
+    }
 }
+
+// Extension for ListNode
+
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.val == rhs.val && lhs.next == rhs.next
+    }
+}
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.034 (0.036) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.removeNthFromEnd(ListNode([1,2,3,4,5]), 2)
+        XCTAssertEqual(value, ListNode([1,2,3,5]))
+    }
+    func test1() {
+        let value = solution.removeNthFromEnd(ListNode([1]), 1)
+        XCTAssertEqual(value, ListNode([]))
+    }
+    func test2() {
+        let value = solution.removeNthFromEnd(ListNode([1,2]), 1)
+        XCTAssertEqual(value, ListNode([1]))
+    }
+}
+
+Tests.defaultTestSuite.run()
