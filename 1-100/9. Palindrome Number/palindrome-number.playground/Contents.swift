f9e21724a4af39d33526e7ea43b0b1ae0a0d86e7
asahiocean <asahiocean@icloud.com>
2021-12-25 00:20:01 +0500

    Refactoring: 9. Palindrome Number
    https://leetcode.com/problems/palindrome-number/

--- 1-100/9. Palindrome Number/palindrome-number.playground/Contents.swift
+++ 1-100/9. Palindrome Number/palindrome-number.playground/Contents.swift
@@ -5,18 +5,17 @@ import Foundation
 
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        if x < 0 { return false }
-        let reverse = { () -> Int in
-            var r = 0, x = x
-            while x != 0 {
-                r = r * 10
-                r = r + x % 10
-                x /= 10
-            }
-            if r < Int32.min || r > Int32.max { return 0 }
-            return r
+        return x < 0 ? false : method(x: x) == x
+    }
+    private func method(x: Int) -> Int {
+        var r = 0
+        var x = x
+        while x != 0 {
+            r = r * 10
+            r = r + x % 10
+            x /= 10
         }
-        return reverse() == x
+        return (r < Int32.min || r > Int32.max) ? 0 : r
     }
 }
 
@@ -28,22 +27,26 @@ import XCTest
 
 class Tests: XCTestCase {
     
-    private let s = Solution()
+    private let solution = Solution()
     
     func test0() {
-        XCTAssertEqual(s.isPalindrome(121), true)
+        let value = solution.isPalindrome(121)
+        XCTAssertEqual(value, true)
     }
     
     func test1() {
-        XCTAssertEqual(s.isPalindrome(-121), false)
+        let value = solution.isPalindrome(-121)
+        XCTAssertEqual(value, false)
     }
     
     func test2() {
-        XCTAssertEqual(s.isPalindrome(10), false)
+        let value = solution.isPalindrome(10)
+        XCTAssertEqual(value, false)
     }
     
     func test3() {
-        XCTAssertEqual(s.isPalindrome(-101), false)
+        let value = solution.isPalindrome(-101)
+        XCTAssertEqual(value, false)
     }
 }
 
