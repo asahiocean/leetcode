26f502c1e3f4cf951a555580fabb15af462d0c04
asahiocean <61920574+asahiocean@users.noreply.github.com>
2021-12-24 08:32:38 +0500

    Update Contents.swift

--- 1-100/77. Combinations/combinations.playground/Contents.swift
+++ 1-100/77. Combinations/combinations.playground/Contents.swift
@@ -8,24 +8,26 @@ class Solution {
         guard k <= n else { return [] }
         guard k != 1 else { return (1...n).map{[$0]} }
         guard k != n else { return [[Int](1...n)] }
-        var ans = [[Int]]()
-        func backtracking(_ currentArr: inout [Int], _ visited: inout Set<Int>,  _  last: Int) {
-            guard currentArr.count != k else {
-                ans.append(currentArr)
+        
+        var current: [Int] = []
+        var visited: Set<Int> = []
+        var result = [[Int]]()
+        
+        func backtracking(_ result: inout [[Int]], _ current: inout [Int], _ visited: inout Set<Int>,  _  last: Int) {
+            guard current.count != k else {
+                result.append(current)
                 return
             }
-            for num in max(1, last)...n {
-                if !visited.contains(num) {
-                    currentArr.append(num)
-                    visited.insert(num)
-                    backtracking(&currentArr, &visited,num)
-                    currentArr.removeLast()
-                    visited.remove(num)
-                }
+            for num in max(1, last)...n where !visited.contains(num) {
+                current.append(num)
+                visited.insert(num)
+                backtracking(&result, &current, &visited,num)
+                current.removeLast()
+                visited.remove(num)
             }
         }
-        var current: [Int] = [], visited: Set<Int> = []
-        backtracking(&current, &visited,0)
-        return ans
+        
+        backtracking(&result, &current, &visited,0)
+        return result
     }
 }
