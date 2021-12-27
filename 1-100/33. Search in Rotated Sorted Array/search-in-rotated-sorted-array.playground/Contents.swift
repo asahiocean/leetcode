dbf91a9701df08e7147e7267d0ae53e950232802
asahiocean <asahiocean@icloud.com>
2021-12-27 09:43:23 +0500

    Update: 33. Search in Rotated Sorted Array

--- 1-100/33. Search in Rotated Sorted Array/search-in-rotated-sorted-array.playground/Contents.swift
+++ 1-100/33. Search in Rotated Sorted Array/search-in-rotated-sorted-array.playground/Contents.swift
@@ -5,16 +5,16 @@ import Foundation
 
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var l = 0, r = nums.count - 1, m = 0
+        var left = 0, right = nums.count - 1, mid = 0
         
-        while l <= r {
-            m = (r - l) / 2 + l
-            let nm = nums[m], nl = nums[l], t = target
-            if nm == t { return m }
+        while left <= right {
+            mid = (right - left) / 2 + left
+            let nm = nums[mid], nl = nums[left], t = target
+            if nm == t { return mid }
             if nm >= nl {
-                nm > t && t >= nl ? (r = m - 1) : (l = m + 1)
+                nm > t && t >= nl ? (right = mid - 1) : (left = mid + 1)
             } else {
-                nm < t && t <= nums[r] ? (l = m + 1) : (r = m - 1)
+                nm < t && t <= nums[right] ? (left = mid + 1) : (right = mid - 1)
             }
         }
         return -1
@@ -26,17 +26,19 @@ import XCTest
 // Executed 3 tests, with 0 failures (0 unexpected) in 0.007 (0.010) seconds
 
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.search([4,5,6,7,0,1,2], 0)
+        let res = solution.search([4,5,6,7,0,1,2], 0)
         XCTAssertEqual(res, 4)
     }
     func test1() {
-        let res = s.search([4,5,6,7,0,1,2], 3)
+        let res = solution.search([4,5,6,7,0,1,2], 3)
         XCTAssertEqual(res, -1)
     }
     func test2() {
-        let res = s.search([1], 0)
+        let res = solution.search([1], 0)
         XCTAssertEqual(res, -1)
     }
 }
