3b3b8a9e0e3d7ac189e6629f56c10052b99188f3
asahiocean <asahiocean@icloud.com>
2021-12-28 05:09:41 +0500

    Update Contents.swift
    https: //leetcode.com/problems/roman-to-integer/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/13. Roman to Integer/roman-to-integer.playground/Contents.swift
+++ 1-100/13. Roman to Integer/roman-to-integer.playground/Contents.swift
@@ -4,15 +4,43 @@ import Foundation
 // https://leetcode.com/problems/roman-to-integer/
 
 class Solution {
-    let dict: [Character:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+    
+    private let dict: [Character:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+    
     func romanToInt(_ s: String) -> Int {
         var out = 0, prev = 0
-        s.forEach({
-            let val = dict[$0] ?? 0
-            if val <= prev { out += prev } else { out -= prev }
+        for i in s {
+            let val = dict[i] ?? 0
+            out += val <= prev ? prev : -prev
             prev = val
-        })
+        }
         out += prev
         return out
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.007 (0.008) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.romanToInt("III")
+        XCTAssertEqual(value, 3)
+    }
+    func test1() {
+        let value = solution.romanToInt("LVIII")
+        XCTAssertEqual(value, 58)
+    }
+    func test2() {
+        let value = solution.romanToInt("MCMXCIV")
+        XCTAssertEqual(value, 1994)
+    }
+}
+
+Tests.defaultTestSuite.run()
