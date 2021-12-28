54a17b4702529c676c1ec57115a787846ddb383c
asahiocean <asahiocean@icloud.com>
2021-12-29 03:01:04 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Hash Table/49. Group Anagrams/group-anagrams.playground/Contents.swift
+++ Hash Table/49. Group Anagrams/group-anagrams.playground/Contents.swift
@@ -5,12 +5,42 @@ import Foundation
 
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        var res = [[String]](), dict = [[Character]:[String]]()
-        strs.forEach {
-            let t = $0.sorted()
-            dict[t] == nil ? (dict[t] = [$0]) : (dict[t]?.append($0))
+        var dict = [[Character]:[String]]()
+        for ch in strs {
+            let abc = ch.sorted()
+            dict[abc] == nil ? (dict[abc] = [ch]) : (dict[abc]?.append(ch))
         }
-        dict.forEach{ res.append($1) }
-        return res
+        return dict.map({$1})
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.041 (0.043) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+        let expected = [["nat","tan"],["bat"],["ate","eat","tea"]]
+        XCTAssertEqual(value.count, expected.count)
+        XCTAssertEqual(value.count, 3)
+        value.forEach {
+            XCTAssertTrue(expected.contains($0.sorted()))
+        }
+    }
+    func test1() {
+        let val = solution.groupAnagrams([""])
+        XCTAssertEqual(val, [[""]])
+    }
+    func test2() {
+        let val = solution.groupAnagrams(["a"])
+        XCTAssertEqual(val, [["a"]])
+    }
+}
+
+Tests.defaultTestSuite.run()
