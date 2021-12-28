6601881c75277fabfc592a6db7b9dc812f4de1c5
asahiocean <asahiocean@icloud.com>
2021-12-28 10:11:13 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Array/Convert Sorted Array to Binary Search Tree/convert-sorted-array-to-binary-search-tree.playground/Contents.swift
+++ Array/Convert Sorted Array to Binary Search Tree/convert-sorted-array-to-binary-search-tree.playground/Contents.swift
@@ -1,18 +1,19 @@
 import Foundation
 
+// 108. Convert Sorted Array to Binary Search Tree
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 class Solution {
     func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
-        func helper(_ left: Int, _ right: Int, _ input: [Int]) -> TreeNode? {
-            guard left <= right else {return nil}
-            let mid = (left + right)/2
-            let root = TreeNode(input[mid])
-            root.left = helper(left,mid - 1, input)
-            root.right = helper(mid + 1, right, input)
-            return root
-        }
         return helper(0, nums.count - 1, nums)
+    }
+    private func helper(_ l: Int, _ r: Int, _ input: [Int]) -> TreeNode? {
+        guard l <= r else { return nil }
+        let mid = (l + r)/2
+        let root = TreeNode(input[mid])
+        root.left = helper(l,mid - 1, input)
+        root.right = helper(mid + 1, r, input)
+        return root
     }
 }
 
@@ -28,22 +29,5 @@ public class TreeNode {
         self.val = val
         self.left = left
         self.right = right
-    }
-    public init?(_ array: [Int?]) {
-        var values = array
-        guard !values.isEmpty, let head = values.removeFirst() else { return nil }
-        val = head; left = nil; right = nil
-        var queue = [self]
-        while !queue.isEmpty {
-            let node = queue.removeFirst()
-            if !values.isEmpty, let val = values.removeFirst() {
-                node.left = .init(val)
-                queue.append(node.left!)
-            }
-            if !values.isEmpty, let val = values.removeFirst() {
-                node.right = .init(val)
-                queue.append(node.right!)
-            }
-        }
     }
 }
