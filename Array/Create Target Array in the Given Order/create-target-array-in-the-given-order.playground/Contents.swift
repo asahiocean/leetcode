93688c1be8e3ed70b71c77aa8d6c89b3574d348a
asahiocean <asahiocean@icloud.com>
2021-12-28 10:26:09 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Array/Create Target Array in the Given Order/create-target-array-in-the-given-order.playground/Contents.swift
+++ Array/Create Target Array in the Given Order/create-target-array-in-the-given-order.playground/Contents.swift
@@ -1,29 +1,37 @@
 import Foundation
 
+// 1389. Create Target Array in the Given Order
 // https://leetcode.com/problems/create-target-array-in-the-given-order/
 
 class Solution {
     func createTargetArray(_ nums: [Int], _ index: [Int]) -> [Int] {
-        var res = [Int]()
-        for i in 0..<nums.count { res.insert(nums[i], at: index[i]) }
-        return res
+        var result = [Int]()
+        for i in 0..<nums.count { result.insert(nums[i], at: index[i]) }
+        return result
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.008 (0.011) seconds
+
 import XCTest
 
-//     Executed 3 tests, with 0 failures (0 unexpected) in 0.008 (0.011) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
-    func testExample1() {
-        XCTAssertEqual(s.createTargetArray([0,1,2,3,4],[0,1,2,2,1]), [0,4,1,3,2]) // success
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.createTargetArray([0,1,2,3,4],[0,1,2,2,1])
+        XCTAssertEqual(value, [0,4,1,3,2])
     }
-    func testExample2() {
-        XCTAssertEqual(s.createTargetArray([1,2,3,4,0],[0,1,2,3,0]), [0,1,2,3,4]) // success
+    func test1() {
+        let value = solution.createTargetArray([1,2,3,4,0],[0,1,2,3,0])
+        XCTAssertEqual(value, [0,1,2,3,4])
     }
-    func testExample3() {
-        XCTAssertEqual(s.createTargetArray([1],[0]), [1]) // success
+    func test2() {
+        let value = solution.createTargetArray([1],[0])
+        XCTAssertEqual(value, [1])
     }
 }
 
