38c12f06bd6fae3d3defcb62aeded36aeeb11c3a
asahiocean <asahiocean@icloud.com>
2021-12-29 10:56:22 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/48. Rotate Image/rotate-image.playground/Contents.swift
+++ 1-100/48. Rotate Image/rotate-image.playground/Contents.swift
@@ -20,30 +20,34 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 4 tests, with 0 failures (0 unexpected) in 0.022 (0.023) seconds
+
 import XCTest
 
-// Executed 4 tests, with 0 failures (0 unexpected) in 0.022 (0.023) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
         var arr = [[1,2,3],[4,5,6],[7,8,9]]
-        s.rotate(&arr)
+        solution.rotate(&arr)
         XCTAssertEqual(arr, [[7,4,1],[8,5,2],[9,6,3]])
     }
     func test1() {
         var arr = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-        s.rotate(&arr)
+        solution.rotate(&arr)
         XCTAssertEqual(arr, [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]])
     }
     func test2() {
         var arr = [[1]]
-        s.rotate(&arr)
+        solution.rotate(&arr)
         XCTAssertEqual(arr, [[1]])
     }
     func test3() {
         var arr = [[1,2],[3,4]]
-        s.rotate(&arr)
+        solution.rotate(&arr)
         XCTAssertEqual(arr, [[3,1],[4,2]])
     }
 }
