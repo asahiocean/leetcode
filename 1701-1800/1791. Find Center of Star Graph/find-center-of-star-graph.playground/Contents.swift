5166a311a3e5a1eb9fe80b358229da0aaa8ba9af
asahiocean <asahiocean@icloud.com>
2021-12-30 20:07:52 +0500

    1791. Find Center of Star Graph
    https: //leetcode.com/problems/find-center-of-star-graph/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1701-1800/1791. Find Center of Star Graph/find-center-of-star-graph.playground/Contents.swift
+++ 1701-1800/1791. Find Center of Star Graph/find-center-of-star-graph.playground/Contents.swift
@@ -10,19 +10,24 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.008 (0.010) seconds
+
 import XCTest
 
-// Executed 2 tests, with 0 failures (0 unexpected) in 0.008 (0.010) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.findCenter([[1,2],[2,3],[4,2]])
-        XCTAssertEqual(res, 2)
+        let value = solution.findCenter([[1,2],[2,3],[4,2]])
+        XCTAssertEqual(value, 2)
     }
+    
     func test1() {
-        let res = s.findCenter([[1,2],[5,1],[1,3],[1,4]])
-        XCTAssertEqual(res, 1)
+        let value = solution.findCenter([[1,2],[5,1],[1,3],[1,4]])
+        XCTAssertEqual(value, 1)
     }
 }
 
