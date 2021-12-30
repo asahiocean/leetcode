b1fcdaa028a89e85b5ba0cb433d6e8e580f5a1ba
asahiocean <asahiocean@icloud.com>
2021-12-30 16:08:43 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/53. Maximum Subarray/maximum-subarray.playground/Contents.swift
+++ 1-100/53. Maximum Subarray/maximum-subarray.playground/Contents.swift
@@ -16,25 +16,26 @@ class Solution {
 
 // MARK: - Test cases -
 
-// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.007 (0.009) seconds
+// valueult: Executed 3 tests, with 0 failuvalue (0 unexpected) in 0.007 (0.009) seconds
 
 import XCTest
 
 class Tests: XCTestCase {
     
-    private let s = Solution()
+    private let solution = Solution()
     
+    /// [4,-1,2,1] has the largest sum = 6.
     func test0() {
-        let res = s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
-        XCTAssertEqual(res, 6)
+        let value = solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+        XCTAssertEqual(value, 6)
     }
     func test1() {
-        let res = s.maxSubArray([1])
-        XCTAssertEqual(res, 1)
+        let value = solution.maxSubArray([1])
+        XCTAssertEqual(value, 1)
     }
     func test2() {
-        let res = s.maxSubArray([5,4,-1,7,8])
-        XCTAssertEqual(res, 23)
+        let value = solution.maxSubArray([5,4,-1,7,8])
+        XCTAssertEqual(value, 23)
     }
 }
 
