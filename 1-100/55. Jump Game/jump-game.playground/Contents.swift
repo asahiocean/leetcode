f828bd9bb3b1ad4fe640ed9ec981257b1d2a68b4
asahiocean <asahiocean@icloud.com>
2021-12-30 16:05:51 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/55. Jump Game/jump-game.playground/Contents.swift
+++ 1-100/55. Jump Game/jump-game.playground/Contents.swift
@@ -13,3 +13,28 @@ class Solution {
         return true
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.007 (0.009) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    /// Jump 1 step from index 0 to 1, then 3 steps to the last index.
+    func test0() {
+        let value = solution.canJump([2,3,1,1,4])
+        XCTAssertEqual(value, true)
+    }
+    
+    /// You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+    func test1() {
+        let value = solution.canJump([3,2,1,0,4])
+        XCTAssertEqual(value, false)
+    }
+}
+
+Tests.defaultTestSuite.run()
