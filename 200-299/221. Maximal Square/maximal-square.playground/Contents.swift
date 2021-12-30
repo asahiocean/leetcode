d85df67e5eb14dd70942eeadd1f8da9c83050f56
asahiocean <asahiocean@icloud.com>
2021-12-30 07:22:55 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 200-299/221. Maximal Square/maximal-square.playground/Contents.swift
+++ 200-299/221. Maximal Square/maximal-square.playground/Contents.swift
@@ -8,7 +8,7 @@ class Solution {
         guard !matrix.isEmpty else { return 0 }
         var length = 0
         let row = matrix.count, col = matrix[0].count
-        var dp = Array<[Int]>(repeating: [Int](repeating: 0, count: col+1), count: row+1)
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: col+1), count: row+1)
         for x in 1...row {
             for y in 1...col where matrix[x-1][y-1] == "1" {
                 dp[x][y] = min(dp[x-1][y], dp[x][y-1], dp[x-1][y-1]) + 1
@@ -19,23 +19,30 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.111 (0.113) seconds
+
 import XCTest
 
-// Executed 3 tests, with 0 failures (0 unexpected) in 0.111 (0.113) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
-        XCTAssertEqual(res, 4)
+        let value = solution.maximalSquare([["1","0","1","0","0"],
+                                            ["1","0","1","1","1"],
+                                            ["1","1","1","1","1"],
+                                            ["1","0","0","1","0"]])
+        XCTAssertEqual(value, 4)
     }
     func test1() {
-        let res = s.maximalSquare([["0","1"],["1","0"]])
-        XCTAssertEqual(res, 1)
+        let value = solution.maximalSquare([["0","1"],["1","0"]])
+        XCTAssertEqual(value, 1)
     }
     func test2() {
-        let res = s.maximalSquare([["0"]])
-        XCTAssertEqual(res, 0)
+        let value = solution.maximalSquare([["0"]])
+        XCTAssertEqual(value, 0)
     }
 }
 
