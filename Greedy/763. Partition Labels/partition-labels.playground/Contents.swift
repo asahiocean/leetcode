4508862603b39fefa12e62511bcb612e0dac37a3
asahiocean <asahiocean@icloud.com>
2022-01-01 10:33:21 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Greedy/763. Partition Labels/partition-labels.playground/Contents.swift
+++ Greedy/763. Partition Labels/partition-labels.playground/Contents.swift
@@ -6,9 +6,9 @@ import Foundation
 class Solution {
     func partitionLabels(_ S: String) -> [Int] {
         guard !S.isEmpty else { return [] }
-
+        
         var map = [Character:Int]()
-
+        
         // making a map on the similarity of the hashtable by which we will search
         for (i,c) in S.enumerated() {
             map[c, default: 0] = i
@@ -19,7 +19,7 @@ class Solution {
         
         for (i,c) in S.enumerated() {
             end = max(end, map[c, default: 0])
-
+            
             guard i == end else { continue }
             result.append(i - num + 1)
             num = i + 1
@@ -28,9 +28,28 @@ class Solution {
     }
 }
 
-func tests() {
-    let s = Solution()
-    assert(s.partitionLabels("ababcbacadefegdehijhklij") == [9,7,8]) // good
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.012 (0.014) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    /// The partition is "ababcbaca", "defegde", "hijhklij".
+    /// This is a partition so that each letter appears in at most one part.
+    /// A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+    func test0() {
+        let value = solution.partitionLabels("ababcbacadefegdehijhklij")
+        XCTAssertEqual(value, [9,7,8])
+    }
+    
+    func test1() {
+        let value = solution.partitionLabels("eccbbbbdec")
+        XCTAssertEqual(value, [10])
+    }
 }
 
-tests()
+Tests.defaultTestSuite.run()
