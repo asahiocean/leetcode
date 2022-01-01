a8897831dc15fe8286af85fe2491af6d980b44bc
asahiocean <asahiocean@icloud.com>
2022-01-01 10:31:41 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Greedy/763. Partition Labels/partition-labels.playground/Contents.swift
+++ Greedy/763. Partition Labels/partition-labels.playground/Contents.swift
@@ -1,11 +1,7 @@
 import Foundation
 
+// 763. Partition Labels
 // https://leetcode.com/problems/partition-labels/
-
-/*
-    A string S of lowercase English letters is given.
-    We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
-*/
 
 class Solution {
     func partitionLabels(_ S: String) -> [Int] {
