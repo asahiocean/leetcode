1ef1d4cc69df8f01101b574f118c0ef320dc9884
asahiocean <asahiocean@icloud.com>
2022-01-01 12:07:00 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/23. Merge k Sorted Lists/merge-k-sorted-lists.playground/Contents.swift
+++ 1-100/23. Merge k Sorted Lists/merge-k-sorted-lists.playground/Contents.swift
@@ -29,6 +29,38 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.031 (0.033) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    /// The linked-lists are:
+    /// [
+    ///   1->4->5,
+    ///   1->3->4,
+    ///   2->6
+    /// ]
+    /// merging them into one sorted list:
+    /// 1->1->2->3->4->4->5->6
+    func test0() {
+        let lists = [ListNode([1, 4, 5]), ListNode([1, 3, 4]), ListNode([2, 6])]
+        let value = solution.mergeKLists(lists)
+        XCTAssertEqual(value, ListNode([1,1,2,3,4,4,5,6]))
+    }
+    
+    func test1() {
+        let value = solution.mergeKLists([ListNode([])])
+        XCTAssertEqual(value, ListNode([]))
+    }
+}
+
+Tests.defaultTestSuite.run()
+
 // MARK: - ListNode -
 
 public class ListNode {
@@ -37,4 +69,24 @@ public class ListNode {
     public init() { self.val = 0; self.next = nil; }
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    // An additional initializer that can be used to implement nodes from an array
+    public init?(_ array: [Int]) {
+        guard !array.isEmpty else { return nil }
+        self.val = array[0]
+        var prev: ListNode = self
+        for i in 1..<array.count {
+            let new = ListNode(array[i])
+            prev.next = new
+            prev = new
+        }
+    }
+}
+
+// Extension for ListNode
+
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.val == rhs.val && lhs.next == rhs.next
+    }
 }
