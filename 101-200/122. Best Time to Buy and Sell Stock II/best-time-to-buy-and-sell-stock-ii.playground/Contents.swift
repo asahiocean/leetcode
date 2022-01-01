66b39617ae1d16e5f44a32af02e135c740d3711f
asahiocean <asahiocean@icloud.com>
2022-01-01 10:46:58 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 101-200/122. Best Time to Buy and Sell Stock II/best-time-to-buy-and-sell-stock-ii.playground/Contents.swift
+++ 101-200/122. Best Time to Buy and Sell Stock II/best-time-to-buy-and-sell-stock-ii.playground/Contents.swift
@@ -14,3 +14,37 @@ class Solution {
         return profit
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.007 (0.009) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    /// Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+    /// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+    /// Total profit is 4 + 3 = 7.
+    func test0() {
+        let value = solution.maxProfit([7,1,5,3,6,4])
+        XCTAssertEqual(value, 7)
+    }
+    
+    /// Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+    /// Total profit is 4.
+    func test1() {
+        let value = solution.maxProfit([1,2,3,4,5])
+        XCTAssertEqual(value, 4)
+    }
+    
+    /// There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+    func test2() {
+        let value = solution.maxProfit([7,6,4,3,1])
+        XCTAssertEqual(value, 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
