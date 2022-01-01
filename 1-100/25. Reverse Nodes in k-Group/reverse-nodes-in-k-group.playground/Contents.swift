c0527b48f16032c372551437d30e5e94437629e1
asahiocean <asahiocean@icloud.com>
2022-01-01 12:38:07 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/25. Reverse Nodes in k-Group/reverse-nodes-in-k-group.playground/Contents.swift
+++ 1-100/25. Reverse Nodes in k-Group/reverse-nodes-in-k-group.playground/Contents.swift
@@ -31,10 +31,55 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.040 (0.042) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.reverseKGroup(ListNode([1,2,3,4,5]), 2)
+        XCTAssertEqual(value, ListNode([2,1,4,3,5]))
+    }
+    
+    func test1() {
+        let value = solution.reverseKGroup(ListNode([1,2,3,4,5]), 3)
+        XCTAssertEqual(value, ListNode([3,2,1,4,5]))
+    }
+}
+
+Tests.defaultTestSuite.run()
+
+// MARK: - ListNode -
+
 public class ListNode {
     public var val: Int
     public var next: ListNode?
     public init() { self.val = 0; self.next = nil; }
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    // An additional initializer that can be used to implement nodes from an array
+    public init?(_ array: [Int]) {
+        guard !array.isEmpty else { return nil }
+        self.val = array[0]
+        var prev: ListNode = self
+        for i in 1..<array.count {
+            let new = ListNode(array[i])
+            prev.next = new
+            prev = new
+        }
+    }
+}
+
+// Extension for ListNode
+
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.val == rhs.val && lhs.next == rhs.next
+    }
 }
