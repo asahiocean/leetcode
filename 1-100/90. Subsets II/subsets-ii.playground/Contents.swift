7ef318b846ec71d41d5af32a786c7bd32d88be45
asahiocean <asahiocean@icloud.com>
2022-01-03 11:09:09 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/90. Subsets II/subsets-ii.playground/Contents.swift
+++ 1-100/90. Subsets II/subsets-ii.playground/Contents.swift
@@ -5,35 +5,38 @@ import Foundation
 
 class Solution {
     func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
-        var res = [[Int]](), path = [Int]()
-        let nums = nums.sorted(by: <)
-        func dfs(_ res: inout [[Int]], _ path: inout [Int], _ nums: [Int], _ index: Int) {
-            res.append(path)
-            for i in index..<nums.count {
-                if i > 0 && nums[i] == nums[i - 1] && i != index { continue }
-                path.append(nums[i])
-                dfs(&res, &path, nums, i + 1)
-                path.removeLast()
-            }
+        var result = [[Int]](), path: [Int] = []
+        dfs(&result, &path, nums.sorted(by: <))
+        return result
+    }
+    private func dfs(_ result: inout [[Int]], _ path: inout [Int], _ nums: [Int], _ index: Int = 0) {
+        result.append(path)
+        for i in index..<nums.count {
+            if i > 0 && nums[i] == nums[i - 1] && i != index { continue }
+            path.append(nums[i])
+            dfs(&result, &path, nums, i + 1)
+            path.removeLast()
         }
-        dfs(&res, &path, nums, 0)
-        return res
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.008 (0.010) seconds
+
 import XCTest
 
-//     Executed 2 tests, with 0 failures (0 unexpected) in 0.008 (0.010) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.subsetsWithDup([1,2,2])
-        XCTAssertEqual(res, [[],[1],[1,2],[1,2,2],[2],[2,2]])
+        let value = solution.subsetsWithDup([1,2,2])
+        XCTAssertEqual(value, [[],[1],[1,2],[1,2,2],[2],[2,2]])
     }
     func test1() {
-        let res = s.subsetsWithDup([0])
-        XCTAssertEqual(res, [[],[0]])
+        let value = solution.subsetsWithDup([0])
+        XCTAssertEqual(value, [[],[0]])
     }
 }
 
