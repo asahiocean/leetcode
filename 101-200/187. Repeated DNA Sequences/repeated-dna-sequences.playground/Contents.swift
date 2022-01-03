349795c00a2e9cb97a8fa1d75c7c454319168912
asahiocean <asahiocean@icloud.com>
2022-01-03 12:08:06 +0500

    187. Repeated DNA Sequences
    https: //leetcode.com/problems/repeated-dna-sequences/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 101-200/187. Repeated DNA Sequences/repeated-dna-sequences.playground/Contents.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+// 187. Repeated DNA Sequences
+// https://leetcode.com/problems/repeated-dna-sequences/
+
+class Solution {
+    func findRepeatedDnaSequences(_ s: String) -> [String] {
+        guard s.count > 10 else { return [] }
+        
+        let ssi = s.startIndex, valid: Set<Character> = ["A", "C", "G", "T"]
+        var str = String(s[ssi...s.index(ssi, offsetBy: 9)].filter{ valid.contains($0) })
+        
+        var result: [String] = []
+        
+        var uniqset: Set<String> = []
+        uniqset.insert(str)
+        
+        let chars = [Character](s)
+        
+        for i in 10..<s.count {
+            str.removeFirst()
+            str.append(chars[i])
+            
+            if result.contains(str) { continue }
+            
+            if uniqset.contains(str) {
+                result.append(str)
+                uniqset.remove(str)
+                continue
+            }
+            uniqset.insert(str)
+        }
+        return [String](result)
+    }
+}
+
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.015 (0.017) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")
+        XCTAssertEqual(value, ["AAAAACCCCC","CCCCCAAAAA"])
+    }
+    
+    func test1() {
+        let value = solution.findRepeatedDnaSequences("AAAAAAAAAAAAA")
+        XCTAssertEqual(value, ["AAAAAAAAAA"])
+    }
+}
+
+Tests.defaultTestSuite.run()
