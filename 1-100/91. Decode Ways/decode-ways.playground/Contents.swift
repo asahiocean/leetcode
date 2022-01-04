581cba7db529804f902b95807ad64f3e20c2e1c2
asahiocean <asahiocean@icloud.com>
2022-01-04 10:30:05 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/91. Decode Ways/decode-ways.playground/Contents.swift
+++ 1-100/91. Decode Ways/decode-ways.playground/Contents.swift
@@ -5,17 +5,51 @@ import Foundation
 
 class Solution {
     func numDecodings(_ s: String) -> Int {
-        let s = Array(s)
-        var dp = Array(repeating: 0, count: s.count + 1)
+        
+        let arrS = Array<Character>(s)
+        var dp = Array(repeating: 0, count: arrS.count + 1)
         dp[0] = 1
-        func isValid(_ chars: [Character], _ s: Int, _ e: Int) -> Bool {
-            let num = Int(String(chars[s...e]))!
-            return num >= 10 && num <= 26
+        
+        for i in 1...arrS.count {
+            if arrS[i-1] != "0" { dp[i] += dp[i-1] }
+            if i > 1 && isValid(arrS, i - 2, i - 1) { dp[i] += dp[i-2] }
         }
-        for i in 1...s.count {
-            if s[i-1] != "0" { dp[i] += dp[i-1] }
-            if i > 1 && isValid(s, i - 2, i - 1) { dp[i] += dp[i-2] }
-        }
-        return dp[s.count]
+        return dp[arrS.count]
+    }
+    
+    private func isValid(_ chars: [Character], _ s: Int, _ e: Int) -> Bool {
+        let num = Int(String(chars[s...e]))!
+        return num >= 10 && num <= 26
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.012 (0.014) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    /// "12" could be decoded as "AB" (1 2) or "L" (12).
+    func test0() {
+        let value = solution.numDecodings("12")
+        XCTAssertEqual(value, 2)
+    }
+    
+    /// "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+    func test1() {
+        let value = solution.numDecodings("226")
+        XCTAssertEqual(value, 3)
+    }
+    
+    /// "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+    func test2() {
+        let value = solution.numDecodings("06")
+        XCTAssertEqual(value, 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
