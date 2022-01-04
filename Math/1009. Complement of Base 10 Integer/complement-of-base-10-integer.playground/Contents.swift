3be0ca03917b86110d5f2375282b4b82d092188b
asahiocean <asahiocean@icloud.com>
2022-01-04 08:37:32 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Math/1009. Complement of Base 10 Integer/complement-of-base-10-integer.playground/Contents.swift
+++ Math/1009. Complement of Base 10 Integer/complement-of-base-10-integer.playground/Contents.swift
@@ -21,14 +21,19 @@ class Tests: XCTestCase {
     
     private let solution = Solution()
     
+    /// 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
     func test1() {
         let value = solution.bitwiseComplement(5)
         XCTAssertEqual(value, 2)
     }
+    
+    /// 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
     func test2() {
         let value = solution.bitwiseComplement(7)
         XCTAssertEqual(value, 0)
     }
+    
+    /// 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
     func test3() {
         let value = solution.bitwiseComplement(10)
         XCTAssertEqual(value, 5)
