2e4322c085cf5be00ea9d5ada976a9cf61ba95bf
asahiocean <asahiocean@icloud.com>
2022-01-04 10:23:57 +0500

    142. Linked List Cycle II
    https: //leetcode.com/problems/linked-list-cycle-ii/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 101-200/142. Linked List Cycle II/linked-list-cycle-ii.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+// 142. Linked List Cycle II
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        
+        var slow: ListNode? = head
+        var fast: ListNode? = head
+        
+        while fast != nil, fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+            if slow === fast { break }
+        }
+        
+        slow = head
+        
+        while slow !== fast {
+            slow = slow?.next
+            fast = fast?.next
+        }
+        return fast == nil || fast?.next == nil ? nil : slow
+    }
+}
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
