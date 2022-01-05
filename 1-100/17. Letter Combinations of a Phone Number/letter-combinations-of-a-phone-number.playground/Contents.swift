25672f0d1c60c1a80725218746ef6b017b725ef3
asahiocean <asahiocean@icloud.com>
2022-01-05 15:44:25 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/17. Letter Combinations of a Phone Number/letter-combinations-of-a-phone-number.playground/Contents.swift
+++ 1-100/17. Letter Combinations of a Phone Number/letter-combinations-of-a-phone-number.playground/Contents.swift
@@ -13,9 +13,9 @@ class Solution {
                                                    "8":["t","u","v"],
                                                    "9":["w","x","y","z"]]
     func letterCombinations(_ digits: String) -> [String] {
-        var digs = digits.map { Character($0.lowercased()) }, result: [String] = []
-        for n in digs {
-            guard let chars = matrix[n] else { break }
+        var result: [String] = []
+        for ch in digits.map({ Character($0.lowercased()) }) {
+            guard let chars = matrix[ch] else { break }
             guard !(result.isEmpty) else {
                 chars.forEach { result.append("\($0)") }
                 continue
@@ -34,7 +34,7 @@ class Solution {
 
 // MARK: - Test cases -
 
-// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.078 (0.080) seconds
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.027 (0.029) seconds
 
 import XCTest
 
