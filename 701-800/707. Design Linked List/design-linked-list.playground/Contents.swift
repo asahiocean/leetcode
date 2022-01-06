5d046f0d1a5ac690329cd965bf39a72419f254f9
asahiocean <asahiocean@icloud.com>
2022-01-06 06:37:26 +0500

    707. Design Linked List
    https: //leetcode.com/problems/design-linked-list/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 701-800/707. Design Linked List/design-linked-list.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+// 707. Design Linked List
+// https://leetcode.com/problems/design-linked-list/
+
+class MyLinkedList {
+    
+    private var items: [Int] = []
+    private var count: Int = 0
+    
+    init() {
+        
+    }
+    
+    func get(_ index: Int) -> Int {
+        return index < 0 || index > (count - 1) ? -1 : items[index]
+    }
+    
+    func addAtHead(_ val: Int) {
+        count += 1
+        items.insert(val, at: 0)
+    }
+    
+    func addAtTail(_ val: Int) {
+        count += 1
+        items.append(val)
+    }
+    
+    func addAtIndex(_ index: Int, _ val: Int) {
+        if index < 0 || index > count { return }
+        count += 1
+        if index == count {
+            items.append(val)
+        } else {
+            items.insert(val, at: index)
+        }
+    }
+    
+    func deleteAtIndex(_ index: Int) {
+        if index < 0 || index >= count { return }
+        count -= 1
+        items.remove(at: index)
+    }
+}
+
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * let obj = MyLinkedList()
+ * let ret_1: Int = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index, val)
+ * obj.deleteAtIndex(index)
+ */
