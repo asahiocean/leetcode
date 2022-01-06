2163c9c8ae8aebd23e9d590c1e3e327419d899f4
asahiocean <asahiocean@icloud.com>
2022-01-06 06:26:59 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/55. Jump Game/jump-game.playground/Contents.swift
+++ 1-100/55. Jump Game/jump-game.playground/Contents.swift
@@ -7,7 +7,7 @@ class Solution {
     func canJump(_ nums: [Int]) -> Bool {
         var indexMax = nums[0]
         for (i, v) in nums.enumerated() {
-            if i > indexMax{ return false }
+            if i > indexMax { return false }
             indexMax = max(indexMax, i + v)
         }
         return true
