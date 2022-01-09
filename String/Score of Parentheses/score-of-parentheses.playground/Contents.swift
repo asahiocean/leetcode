b4e7ee279bc519c181be46603fd34e5d5acd63fd
asahiocean <asahiocean@icloud.com>
2022-01-10 03:12:42 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/Score of Parentheses/score-of-parentheses.playground/Contents.swift
+++ String/Score of Parentheses/score-of-parentheses.playground/Contents.swift
@@ -1,40 +1,55 @@
 import Foundation
 
+// 856. Score of Parentheses
 // https://leetcode.com/problems/score-of-parentheses/
 
 class Solution {
+    private let parL: Character = "("
     func scoreOfParentheses(_ s: String) -> Int {
+        
         var value = 0, result = 0
-        let S = Array(s)
-        for i in 0 ..< S.count {
-            if S[i] == "(" {
+        let array = Array(s)
+        
+        for i in 0 ..< array.count {
+            if array[i] == parL {
                 value += 1
             } else {
                 value -= 1
-                if S[i-1] == "(" { result += 1 << value }
+                if array[i-1] == parL { result += 1 << value }
             }
         }
         return result
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 4 tests, with 0 failures (0 unexpected) in 0.008 (0.009) seconds
+
 import XCTest
 
-// Executed 4 tests, with 0 failures (0 unexpected) in 0.008 (0.009) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test1() {
-        XCTAssertEqual(s.scoreOfParentheses("()"), 1)
+        let value = solution.scoreOfParentheses("()")
+        XCTAssertEqual(value, 1)
     }
+    
     func test2() {
-        XCTAssertEqual(s.scoreOfParentheses("(())"), 2)
+        let value = solution.scoreOfParentheses("(())")
+        XCTAssertEqual(value, 2)
     }
+    
     func test3() {
-        XCTAssertEqual(s.scoreOfParentheses("()()"), 2)
+        let value = solution.scoreOfParentheses("()()")
+        XCTAssertEqual(value, 2)
     }
+    
     func test4() {
-        XCTAssertEqual(s.scoreOfParentheses("(()(()))"), 6)
+        let value = solution.scoreOfParentheses("(()(()))")
+        XCTAssertEqual(value, 6)
     }
 }
 
