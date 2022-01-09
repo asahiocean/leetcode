7f23cd2076730de9e0cc89ada08e60e4e8ee27b9
asahiocean <asahiocean@icloud.com>
2022-01-10 03:00:08 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/Magical String/magical-string.playground/Contents.swift
+++ String/Magical String/magical-string.playground/Contents.swift
@@ -1,37 +1,43 @@
 import Foundation
 
-// https://leetcode.com/problems/magical-string
+// 481. Magical String
+// https://leetcode.com/problems/magical-string/
 
 class Solution {
     func magicalString(_ n: Int) -> Int {
         if n == 0 { return 0 }
-        var queue = [1, 2, 2], i = 2
+        
+        var queue = [1,2,2], num = 2
+        
         while queue.count < n {
-            let add = queue.last == 1 ? 2 : 1
-            if queue[i] == 1 {
-                queue.append(add)
-            } else {
-                queue.append(add)
-                queue.append(add)
-            }
-            i += 1
+            let val = queue.last == 1 ? 2 : 1
+            func add() { queue.append(val) }
+            if queue[num] == 1 { add() } else { add(); add() }
+            num += 1
         }
         if queue.count > n { queue.removeLast() }
         return queue.reduce(0, { $1 == 1 ? $0 + 1 : $0 })
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
+
 import XCTest
 
-//      Executed 2 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test1() {
-        XCTAssertEqual(s.magicalString(6), 3)
+        let value = solution.magicalString(6)
+        XCTAssertEqual(value, 3)
     }
+    
     func test2() {
-        XCTAssertEqual(s.magicalString(3), 1)
+        let value = solution.magicalString(3)
+        XCTAssertEqual(value, 1)
     }
 }
 
