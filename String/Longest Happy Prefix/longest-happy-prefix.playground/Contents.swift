e7a6e37fca461d02c19e945520a50a80d83f5b17
asahiocean <asahiocean@icloud.com>
2022-01-10 02:25:02 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/Longest Happy Prefix/longest-happy-prefix.playground/Contents.swift
+++ String/Longest Happy Prefix/longest-happy-prefix.playground/Contents.swift
@@ -1,21 +1,53 @@
 import Foundation
 
 // 1392. Longest Happy Prefix
-// Runtime: 388 ms, faster than 100.00%
 // https://leetcode.com/problems/longest-happy-prefix/
 
 class Solution {
     func longestPrefix(_ s: String) -> String {
-        guard s.count > 1 else { return "" }
-        let l = s.count, chars = [Character](s)
-        var next = [Int](repeating: -1, count: l)
+        let len = s.count
+        guard len > 1 else { return "" }
         
-        var k = -1
-        for i in 1..<l {
-            while k != -1 && chars[k+1] != chars[i] { k = next[k] }
-            if chars[k + 1] == chars[i] { k += 1 }
-            next[i] = k
+        let chars = [Character](s)
+        var next = [Int](repeating: -1, count: len)
+        
+        var num = -1
+        
+        for i in 1..<len {
+            let val = chars[i]
+            
+            while num != -1 && chars[num + 1] != val {
+                num = next[num]
+            }
+            if chars[num + 1] == val { num += 1 }
+            next[i] = num
         }
-        return next[l-1] == -1 ? "" : String(chars[0...next[l-1]])
+        return next[len-1] == -1 ? "" : String(chars[0...next[len-1]])
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.012 (0.014) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    // s contains 4 prefix excluding itself ("l", "le", "lev", "leve"), and suffix ("l", "el", "vel", "evel").
+    // The largest prefix which is also suffix is given by "l".
+    func test0() {
+        let value = solution.longestPrefix("level")
+        XCTAssertEqual(value, "l")
+    }
+    
+    // "abab" is the largest prefix which is also suffix. They can overlap in the original string.
+    func test1() {
+        let value = solution.longestPrefix("ababab")
+        XCTAssertEqual(value, "abab")
+    }
+}
+
+Tests.defaultTestSuite.run()
