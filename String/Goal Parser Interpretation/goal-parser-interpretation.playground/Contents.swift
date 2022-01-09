230b8ce7105d0613202082b959e89cc13fa03537
asahiocean <asahiocean@icloud.com>
2022-01-10 01:56:38 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
+++ String/Goal Parser Interpretation/goal-parser-interpretation.playground/Contents.swift
@@ -1,29 +1,44 @@
 import Foundation
 
+// 1678. Goal Parser Interpretation
 // https://leetcode.com/problems/goal-parser-interpretation/
 
 class Solution {
     func interpret(_ command: String) -> String {
         if !(1...100).contains(command.count) { return "" }
-        let o = command.replacingOccurrences(of: "()", with: "o")
-        return o.replacingOccurrences(of: "(al)", with: "al")
+        let word = command.replacingOccurrences(of: "()", with: "o")
+        return word.replacingOccurrences(of: "(al)", with: "al")
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.005 (0.035) seconds
+
 import XCTest
 
-//      Executed 3 tests, with 0 failures (0 unexpected) in 0.005 (0.035) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
-    func testExample1() {
-        XCTAssertEqual(s.interpret("G()(al)"),"Goal") // success
+    
+    private let solution = Solution()
+    
+    // The Goal Parser interprets the command as follows:
+    // G -> G
+    // () -> o
+    // (al) -> al
+    // The final concatenated result is "Goal".
+    func test0() {
+        let value = solution.interpret("G()(al)")
+        XCTAssertEqual(value, "Goal")
     }
-    func testExample2() {
-        XCTAssertEqual(s.interpret("G()()()()(al)"),"Gooooal") // success
+    
+    func test1() {
+        let value = solution.interpret("G()()()()(al)")
+        XCTAssertEqual(value, "Gooooal")
     }
-    func testExample3() {
-        XCTAssertEqual(s.interpret("(al)G(al)()()G"),"alGalooG") // success
+    
+    func test3() {
+        let value = solution.interpret("(al)G(al)()()G")
+        XCTAssertEqual(value, "alGalooG")
     }
 }
 
