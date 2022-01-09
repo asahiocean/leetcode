16d5fb9019abc0594e9279561d3a1797a3b12d5d
asahiocean <asahiocean@icloud.com>
2022-01-10 03:28:27 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/Valid Palindrome/valid-palindrome.playground/Contents.swift
+++ String/Valid Palindrome/valid-palindrome.playground/Contents.swift
@@ -6,12 +6,44 @@ import Foundation
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
         let chars = [Character](s.lowercased().filter { $0.isLetter || $0.isNumber })
-        var lIndex = 0, rIndex = (chars.count - 1)
-        while lIndex < rIndex {
-            if chars[lIndex] != chars[rIndex] { return false }
-            lIndex += 1
-            rIndex -= 1
+        var idxL = 0, idxR = (chars.count - 1)
+        while idxL < idxR {
+            if chars[idxL] != chars[idxR] { return false }
+            idxL += 1
+            idxR -= 1
         }
         return true
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.021 (0.023) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    // "amanaplanacanalpanama" is a palindrome.
+    func test0() {
+        let value = solution.isPalindrome("A man, a plan, a canal: Panama")
+        XCTAssertTrue(value)
+    }
+    
+    // "raceacar" is not a palindrome.
+    func test1() {
+        let value = solution.isPalindrome("race a car")
+        XCTAssertFalse(value)
+    }
+    
+    // 's' is an empty string "" after removing non-alphanumeric characters.
+    // Since an empty string reads the same forward and backward, it is a palindrome.
+    func test2() {
+        let value = solution.isPalindrome(" ")
+        XCTAssertTrue(value)
+    }
+}
+
+Tests.defaultTestSuite.run()
