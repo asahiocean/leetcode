1d81bf55cad69d5cc675bbf03a07c0ac1d03fe0d
asahiocean <asahiocean@icloud.com>
2022-01-10 18:38:13 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/64. Minimum Path Sum/minimum-path-sum.playground/Contents.swift
+++ 1-100/64. Minimum Path Sum/minimum-path-sum.playground/Contents.swift
@@ -5,34 +5,41 @@ import Foundation
 
 class Solution {
     func minPathSum(_ grid: [[Int]]) -> Int {
-        var dp = grid
-        let rs = grid.count, cs = grid[0].count
-        (1..<cs).forEach {
-            dp[0][$0] += dp[0][$0-1]
+        var grid = grid
+        let rows = grid.count, cols = grid[0].count
+        for c in 1..<cols {
+            grid[0][c] += grid[0][c-1]
         }
-        (1..<rs).forEach {
-            dp[$0][0] += dp[$0-1][0]
-            for i in 1..<cs {
-                dp[$0][i] += min(dp[$0-1][i], dp[$0][i-1])
+        for r in 1..<rows {
+            grid[r][0] += grid[r-1][0]
+            for c in 1..<cols {
+                let min = min(grid[r-1][c], grid[r][c-1])
+                grid[r][c] += min
             }
         }
-        return dp[rs-1][cs-1]
+        return grid[rows-1][cols-1]
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.036 (0.038) seconds
+
 import XCTest
 
-// Executed 2 tests, with 0 failures (0 unexpected) in 0.036 (0.038) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
+    // Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
     func test0() {
-        let res = s.minPathSum([[1,3,1],[1,5,1],[4,2,1]])
-        XCTAssertEqual(res, 7)
+        let value = solution.minPathSum([[1,3,1],[1,5,1],[4,2,1]])
+        XCTAssertEqual(value, 7)
     }
+    
     func test1() {
-        let res = s.minPathSum([[1,2,3],[4,5,6]])
-        XCTAssertEqual(res, 12)
+        let value = solution.minPathSum([[1,2,3],[4,5,6]])
+        XCTAssertEqual(value, 12)
     }
 }
 
