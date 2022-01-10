55579bd7a833f04fe4d5be27e696cc1e218f472c
asahiocean <asahiocean@icloud.com>
2022-01-10 05:10:08 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/67. Add Binary/add-binary.playground/Contents.swift
+++ 1-100/67. Add Binary/add-binary.playground/Contents.swift
@@ -5,13 +5,13 @@ import Foundation
 
 class Solution {
     func addBinary(_ a: String, _ b: String) -> String {
-        let a = Array(a), al = a.count, b = Array(b), bl = b.count
-        var result = "", carry = 0, i = (al - 1), j = (bl - 1)
+        let arrA = Array(a), lenA = arrA.count, arrB = Array(b), lenB = arrB.count
+        var result = "", carry = 0, i = (lenA - 1), j = (lenB - 1)
         
         while i >= 0 || j >= 0 || carry > 0 {
             var sum = carry
-            i >= 0 ? (sum += Int("\(a[i])")!, i -= 1) : nil
-            j >= 0 ? (sum += Int("\(b[j])")!, j -= 1) : nil
+            i >= 0 ? (sum += Int("\(arrA[i])")!, i -= 1) : nil
+            j >= 0 ? (sum += Int("\(arrB[j])")!, j -= 1) : nil
             result = "\(sum % 2)" + result
             carry = sum / 2
         }
@@ -19,19 +19,24 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.034 (0.036) seconds
+
 import XCTest
 
-// Executed 2 tests, with 0 failures (0 unexpected) in 0.034 (0.036) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.addBinary("11", "1")
-        XCTAssertEqual(res, "100")
+        let value = solution.addBinary("11", "1")
+        XCTAssertEqual(value, "100")
     }
+    
     func test1() {
-        let res = s.addBinary("1010", "1011")
-        XCTAssertEqual(res, "10101")
+        let value = solution.addBinary("1010", "1011")
+        XCTAssertEqual(value, "10101")
     }
 }
 
