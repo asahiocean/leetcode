bcf1c48015c8e17043a0f028b0e76beda0f8892a
asahiocean <asahiocean@icloud.com>
2022-01-11 08:04:34 +0500

    173. Binary Search Tree Iterator
    https: //leetcode.com/problems/binary-search-tree-iterator/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 101-200/173. Binary Search Tree Iterator/binary-search-tree-iterator.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+// 173. Binary Search Tree Iterator
+// https://leetcode.com/problems/binary-search-tree-iterator/
+
+class BSTIterator {
+    
+    private var stack: [TreeNode]
+    
+    init(_ root: TreeNode?) {
+        self.stack = [TreeNode]()
+        loadAllLefts(root)
+    }
+    
+    func next() -> Int {
+        let node = stack.removeLast()
+        loadAllLefts(node.right)
+        return node.val
+    }
+    
+    func hasNext() -> Bool {
+        return !stack.isEmpty
+    }
+    
+    private func loadAllLefts(_ root: TreeNode?) {
+        var node = root
+        while let curr = node {
+            stack.append(curr)
+            node = curr.left
+        }
+    }
+}
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * let obj = BSTIterator(root)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
