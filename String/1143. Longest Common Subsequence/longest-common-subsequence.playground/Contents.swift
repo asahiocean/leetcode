84991223231496bf95ebcc9e64cdff7a832a1d0d
asahiocean <asahiocean@icloud.com>
2022-01-11 07:31:54 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- String/1143. Longest Common Subsequence/longest-common-subsequence.playground/Contents.swift
+++ String/1143. Longest Common Subsequence/longest-common-subsequence.playground/Contents.swift
@@ -5,14 +5,17 @@ import Foundation
 
 class Solution {
     func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
-        if text1.isEmpty || text2.isEmpty { return 0 }
-        guard 1 <= text1.count && text2.count <= 1000 else { return 0 }
-        let a = Array(text1), b = Array(text2)
         var result = 0
-        var dp = Array<[Int]>(repeating: [Int](repeating: 0, count: b.count+1), count: a.count+1)
-        for i in 1...a.count {
-            for j in 1...b.count {
-                dp[i][j] = a[i-1] == b[j-1] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
+        
+        if text1.isEmpty || text2.isEmpty { return result }
+        guard 1 <= text1.count && text2.count <= 1000 else { return result }
+        
+        let arrT1 = Array(text1), arrT2 = Array(text2)
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: arrT2.count + 1), count: arrT1.count + 1)
+        
+        for i in 1...arrT1.count {
+            for j in 1...arrT2.count {
+                dp[i][j] = arrT1[i-1] == arrT2[j-1] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
                 result = max(result, dp[i][j])
             }
         }
@@ -20,23 +23,29 @@ class Solution {
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.048 (0.050) seconds
+
 import XCTest
 
-// Executed 3 tests, with 0 failures (0 unexpected) in 0.048 (0.050) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.longestCommonSubsequence("abcde", "ace")
-        XCTAssertEqual(res, 3)
+        let value = solution.longestCommonSubsequence("abcde", "ace")
+        XCTAssertEqual(value, 3)
     }
+    
     func test1() {
-        let res = s.longestCommonSubsequence("abc", "abc")
-        XCTAssertEqual(res, 3)
+        let value = solution.longestCommonSubsequence("abc", "abc")
+        XCTAssertEqual(value, 3)
     }
+    
     func test2() {
-        let res = s.longestCommonSubsequence("abc", "def")
-        XCTAssertEqual(res, 0)
+        let value = solution.longestCommonSubsequence("abc", "def")
+        XCTAssertEqual(value, 0)
     }
 }
 
