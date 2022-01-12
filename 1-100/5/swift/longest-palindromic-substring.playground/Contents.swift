973c8b58d0588d854a6831d367d324f7837ce5c5
asahiocean <asahiocean@icloud.com>
2022-01-12 06:37:26 +0500

    5. Longest Palindromic Substring
    https: //leetcode.com/problems/longest-palindromic-substring/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 1-100/5/swift/longest-palindromic-substring.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// 5. Longest Palindromic Substring
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        
+    }
+}
