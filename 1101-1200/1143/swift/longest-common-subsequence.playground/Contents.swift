4caec1eaaa0add6371e5a8d0b76b69c861df8475
asahiocean <asahiocean@icloud.com>
2022-01-12 05:17:57 +0500

    Swift solution for: 1143. Longest Common Subsequence
    https: //leetcode.com/problems/longest-common-subsequence/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 1101-1200/1143/swift/longest-common-subsequence.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+// 1143. Longest Common Subsequence
+// https://leetcode.com/problems/longest-common-subsequence/
+
+class Solution {
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        
+        if text1.isEmpty || text2.isEmpty { return 0 }
+        guard 1 <= text1.count && text2.count <= 1000 else { return 0 }
+        
+        var result = 0
+        
+        let arrText1 = Array(text1), arrText2 = Array(text2)
+        let lenArr1 = arrText1.count, lenArr2 = arrText2.count
+        
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: lenArr2 + 1), count: lenArr1 + 1)
+        
+        for i in 1...lenArr1 {
+            for j in 1...lenArr2 {
+                dp[i][j] = arrText1[i-1] == arrText2[j-1] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
+                result = max(result, dp[i][j])
+            }
+        }
+        return result
+    }
+}
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.048 (0.050) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.longestCommonSubsequence("abcde", "ace")
+        XCTAssertEqual(value, 3)
+    }
+    
+    func test1() {
+        let value = solution.longestCommonSubsequence("abc", "abc")
+        XCTAssertEqual(value, 3)
+    }
+    
+    func test2() {
+        let value = solution.longestCommonSubsequence("abc", "def")
+        XCTAssertEqual(value, 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
