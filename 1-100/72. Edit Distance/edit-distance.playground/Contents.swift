3c88f21fd513a39ddc48571a57b4ad4ff513cef9
asahiocean <asahiocean@icloud.com>
2022-01-12 05:44:17 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/72. Edit Distance/edit-distance.playground/Contents.swift
+++ 1-100/72. Edit Distance/edit-distance.playground/Contents.swift
@@ -5,24 +5,59 @@ import Foundation
 
 class Solution {
     func minDistance(_ word1: String, _ word2: String) -> Int {
-        let M = word1.count, N = word2.count
-        guard !word1.isEmpty && !word2.isEmpty else { return max(N,M) }
-        let w1ch = [Character](word1), w2ch = [Character](word2)
-        var editDist = Array<[Int]>(repeating: [Int](repeating: 0, count: N), count: M)
         
-        for j in 0..<N {
-            editDist[0][j] = w1ch[0] == w2ch[j] ? j : (j == 0 ? 0 : editDist[0][j-1]) + 1
+        let lenW1 = word1.count, lenW2 = word2.count
+        
+        guard lenW1 > 0 && lenW2 > 0 else { return max(lenW2,lenW1) }
+        
+        let arrW1ch = [Character](word1), arrW2ch = [Character](word2)
+        
+        var editDist = [[Int]](repeating: [Int](repeating: 0, count: lenW2), count: lenW1)
+        
+        for j in 0..<lenW2 {
+            editDist[0][j] = arrW1ch[0] == arrW2ch[j] ? j : (j == 0 ? 0 : editDist[0][j-1]) + 1
         }
-        for i in 0..<M {
-            editDist[i][0] = w1ch[i] == w2ch[0] ? i : (1 + (i == 0 ? 0 : editDist[i - 1][0]))
+        for i in 0..<lenW1 {
+            editDist[i][0] = arrW1ch[i] == arrW2ch[0] ? i : (1 + (i == 0 ? 0 : editDist[i - 1][0]))
         }
-        for i in 1..<M {
-            for j in 1..<N {
+        for i in 1..<lenW1 {
+            for j in 1..<lenW2 {
                 editDist[i][j] = min(editDist[i-1][j] + 1,
                                      editDist[i][j-1] + 1,
-                                     editDist[i-1][j-1] + (w2ch[j] == w1ch[i] ? 0 : 1))
+                                     editDist[i-1][j-1] + (arrW2ch[j] == arrW1ch[i] ? 0 : 1))
             }
         }
-        return editDist[M-1][N-1]
+        return editDist[lenW1-1][lenW2-1]
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.010 (0.012) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    // horse -> rorse (replace 'h' with 'r')
+    // rorse -> rose (remove 'r')
+    // rose -> ros (remove 'e')
+    func test0() {
+        let value = solution.minDistance("horse", "ros")
+        XCTAssertEqual(value, 3)
+    }
+    
+    // intention -> inention (remove 't')
+    // inention -> enention (replace 'i' with 'e')
+    // enention -> exention (replace 'n' with 'x')
+    // exention -> exection (replace 'n' with 'c')
+    // exection -> execution (insert 'u')
+    func test1() {
+        let value = solution.minDistance("intention", "execution")
+        XCTAssertEqual(value, 5)
+    }
+}
+
+Tests.defaultTestSuite.run()
