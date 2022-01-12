a2223b83712032b1955a7cac587041dce0345e83
asahiocean <asahiocean@icloud.com>
2022-01-12 07:01:35 +0500

    376. Wiggle Subsequence
    https: //leetcode.com/problems/wiggle-subsequence/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 301-400/376/swift/wiggle-subsequence.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// 376. Wiggle Subsequence
+// https://leetcode.com/problems/wiggle-subsequence/
+
+class Solution {
+    func wiggleMaxLength(_ nums: [Int]) -> Int {
+        
+    }
+}
