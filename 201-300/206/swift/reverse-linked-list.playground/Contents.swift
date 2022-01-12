53ee166bbede7b17593fe2af4a0a1481436c35d9
asahiocean <asahiocean@icloud.com>
2022-01-12 09:53:54 +0500

    Swift solution: 206. Reverse Linked List
    https: //leetcode.com/problems/reverse-linked-list/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 201-300/206/swift/reverse-linked-list.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        guard let node = head else { return nil }
+        
+        if node.next == nil { return node }
+        
+        let newHead = reverseList(node.next)
+        let next = node.next
+        
+        next?.next = node
+        node.next = nil
+        return newHead
+    }
+}
+
+// MARK: - ListNode -
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
