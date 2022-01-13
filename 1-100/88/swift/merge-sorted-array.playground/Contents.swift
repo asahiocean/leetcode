2e2aad43ced1249d02f18f85e550dcb353d39165
asahiocean <asahiocean@icloud.com>
2022-01-14 04:27:09 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/88/swift/merge-sorted-array.playground/Contents.swift
+++ 1-100/88/swift/merge-sorted-array.playground/Contents.swift
@@ -29,18 +29,26 @@ class Tests: XCTestCase {
     
     private let solution = Solution()
     
+    // The arrays we are merging are [1,2,3] and [2,5,6].
+    // The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
     func test0() {
         var array = [1,2,3,0,0,0]
         solution.merge(&array, 3, [2,5,6], 3)
         XCTAssertEqual(array, [1,2,2,3,5,6])
     }
     
+    // The arrays we are merging are [1] and [].
+    // The result of the merge is [1].
     func test1() {
         var array = [1]
         solution.merge(&array, 1, [], 0)
         XCTAssertEqual(array, [1])
     }
     
+    // The arrays we are merging are [] and [1].
+    // The result of the merge is [1].
+    // Note that because m = 0, there are no elements in nums1.
+    // The 0 is only there to ensure the merge result can fit in nums1.
     func test2() {
         var array = [0]
         solution.merge(&array, 0, [1], 1)
