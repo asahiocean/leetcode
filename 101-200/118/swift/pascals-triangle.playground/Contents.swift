8ddf77dbe7272f2367f1cb7575f4d1480c4edb56
asahiocean <asahiocean@icloud.com>
2022-01-14 04:57:26 +0500

    Swift solution: 118. Pascal's Triangle
    https: //leetcode.com/problems/pascals-triangle/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 101-200/118/swift/pascals-triangle.playground/Contents.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+// 118. Pascal's Triangle
+// https://leetcode.com/problems/pascals-triangle/
+
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        
+        guard (1...30).contains(numRows) else { return [] }
+        
+        var result: [[Int]] = []
+                
+        for i in 1...numRows {
+            var row = [Int](repeating: 1, count: i)
+            if i <= 2 {
+                result.append(row)
+            } else if let rowLast = result.last {
+                for k in 1...(i / 2) {
+                    row[k] = rowLast[k-1] + rowLast[k]
+                    row[i - k - 1] = row[k]
+                }
+                result.append(row)
+            }
+        }
+        return result
+    }
+}
+
+// MARK: - Test cases -
+
+// Result: Executed 2 tests, with 0 failures (0 unexpected) in 0.010 (0.012) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func test0() {
+        let value = solution.generate(5)
+        XCTAssertEqual(value, [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
+    }
+    
+    func test1() {
+        let value = solution.generate(1)
+        XCTAssertEqual(value, [[1]])
+    }
+}
+
+Tests.defaultTestSuite.run()
