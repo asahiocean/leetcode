41abccadc67f7eb5d705ea2483df75d87a28dc8e
asahiocean <asahiocean@icloud.com>
2022-01-14 08:31:20 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 201-300/232/swift/implement-queue-using-stacks.playground/Contents.swift
+++ 201-300/232/swift/implement-queue-using-stacks.playground/Contents.swift
@@ -42,9 +42,8 @@ import XCTest
 
 class Tests: XCTestCase {
     
-    private let myQueue = MyQueue()
-    
     func test0() {
+        let myQueue = MyQueue()
         myQueue.push(1)     // queue is: [1]
         myQueue.push(2)     // queue is: [1, 2] (leftmost is front of the queue)
         myQueue.peek()      // return 1
