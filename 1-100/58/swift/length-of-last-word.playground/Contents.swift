b0af79b6118e28c4d4aae6c6a9cd8ffcd100159c
asahiocean <asahiocean@icloud.com>
2022-02-02 13:31:32 +0500

    Swift solution: 58. Length of Last Word
    https: //leetcode.com/problems/length-of-last-word/
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 1-100/58/swift/length-of-last-word.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+// 58. Length of Last Word
+// https://leetcode.com/problems/length-of-last-word/
+
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        guard let word = s.split(separator: " ").last else { return 0 }
+        return word.count
+    }
+}
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.010 (0.012) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+    
+    private let solution = Solution()
+    
+    // The last word is "World" with length 5.
+    func test0() {
+        let value = solution.lengthOfLastWord("Hello World")
+        XCTAssertEqual(value, 5)
+    }
+    
+    // The last word is "moon" with length 4.
+    func test1() {
+        let value = solution.lengthOfLastWord("   fly me   to   the moon  ")
+        XCTAssertEqual(value, 4)
+    }
+    
+    // The last word is "joyboy" with length 6.
+    func test2() {
+        let value = solution.lengthOfLastWord("luffy is still joyboy")
+        XCTAssertEqual(value, 6)
+    }
+}
+
+Tests.defaultTestSuite.run()
