0c4e7b561af73f44bd2869d9e660e20ff6d98c17
asahiocean <61920574+asahiocean@users.noreply.github.com>
2022-02-27 00:23:08 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 101-200/191/swift/number-of-1-bits.playground/Contents.swift
+++ 101-200/191/swift/number-of-1-bits.playground/Contents.swift
@@ -11,7 +11,7 @@ class Solution {
 
 // MARK: - Test cases -
 
-// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.025 (0.041) seconds
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.005 (0.007) seconds
 
 import XCTest
 
@@ -22,37 +22,26 @@ class Tests: XCTestCase {
     // The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
     func test0() {
         let binary = "00000000000000000000000000001011"
-        if let number = Int(binary, radix: 2) {
-            let value = solution.hammingWeight(number)
-            XCTAssertEqual(value, 3)
-        }
-        let number: UInt = strtoul(binary, nil, 2)
-        let value = solution.hammingWeight(Int(number))
+        let value = solution.hammingWeight(int(binary))
         XCTAssertEqual(value, 3)
     }
     
     // The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
     func test1() {
         let binary = "00000000000000000000000010000000"
-        if let number = Int(binary, radix: 2) {
-            let value = solution.hammingWeight(number)
-            XCTAssertEqual(value, 1)
-        }
-        let number: UInt = strtoul(binary, nil, 2)
-        let value = solution.hammingWeight(Int(number))
+        let value = solution.hammingWeight(int(binary))
         XCTAssertEqual(value, 1)
     }
     
     // The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
     func test2() {
         let binary = "11111111111111111111111111111101"
-        if let number = Int(binary, radix: 2) {
-            let value = solution.hammingWeight(number)
-            XCTAssertEqual(value, 31)
-        }
-        let number: UInt = strtoul(binary, nil, 2)
-        let value = solution.hammingWeight(Int(number))
+        let value = solution.hammingWeight(int(binary))
         XCTAssertEqual(value, 31)
+    }
+    
+    private func int(_ binary: String) -> Int {
+        return Int(strtoul(binary, nil, 2))
     }
 }
 
