e43cf85458c7aa9e9a5672fc9f6d974cee547af9
asahiocean <61920574+asahiocean@users.noreply.github.com>
2022-03-04 02:27:38 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 801-900/841/swift/keys-and-rooms.playground/Contents.swift
+++ 801-900/841/swift/keys-and-rooms.playground/Contents.swift
@@ -6,20 +6,19 @@ import Foundation
 class Solution {
     func canVisitAllRooms(_ rooms: [[Int]]) -> Bool {
         
-        var visited: Set<Int> = []
-        var roomsQueue: [Int] = []
-        roomsQueue.append(0)
+        var visit: Set<Int> = []
+        var queue: [Int] = [0]
         
-        while !roomsQueue.isEmpty {
-            for _ in 0..<roomsQueue.count {
-                let room = roomsQueue.removeFirst()
-                visited.insert(room)
-                for r in rooms[room] where !visited.contains(r) {
-                    roomsQueue.append(r)
+        while !queue.isEmpty {
+            for _ in 0..<queue.count {
+                let room = queue.removeFirst()
+                visit.insert(room)
+                for r in rooms[room] where !visit.contains(r) {
+                    queue.append(r)
                 }
             }
         }
-        return visited.count == rooms.count
+        return visit.count == rooms.count
     }
 }
 
@@ -33,11 +32,13 @@ class Tests: XCTestCase {
     
     private let solution = Solution()
     
-    // We visit room 0 and pick up key 1.
-    // We then visit room 1 and pick up key 2.
-    // We then visit room 2 and pick up key 3.
-    // We then visit room 3.
-    // Since we were able to visit every room, we return true.
+    /*
+       We visit room 0 and pick up key 1.
+       We then visit room 1 and pick up key 2.
+       We then visit room 2 and pick up key 3.
+       We then visit room 3.
+       Since we were able to visit every room, we return true.
+    */
     func test0() {
         let value = solution.canVisitAllRooms([[1],[2],[3],[]])
         XCTAssertEqual(value, true)
