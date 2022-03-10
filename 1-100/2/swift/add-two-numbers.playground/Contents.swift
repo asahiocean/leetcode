e5a338e3b02e7854dc113fda8ebed2c9961e750c
asahiocean <61920574+asahiocean@users.noreply.github.com>
2022-03-10 06:20:04 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/2/swift/add-two-numbers.playground/Contents.swift
+++ 1-100/2/swift/add-two-numbers.playground/Contents.swift
@@ -4,13 +4,12 @@ import Foundation
 // https://leetcode.com/problems/add-two-numbers/
 
 class Solution {
-    private var anchor = 0
+    private var digit = 0
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        if l1 == nil && l2 == nil && anchor == 0 { return nil }
-        let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + anchor
-        anchor = sum / 10
-        let node: ListNode? = ListNode(sum % 10, addTwoNumbers(l1?.next, l2?.next))
-        return node
+        if l1 == nil && l2 == nil && digit == 0 { return nil }
+        let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + digit
+        digit = sum / 10
+        return ListNode(sum % 10, addTwoNumbers(l1?.next, l2?.next))
     }
 }
 
@@ -28,30 +27,18 @@ class Tests: XCTestCase {
     func test0() {
         let node = solution.addTwoNumbers(ListNode([2,4,3]), ListNode([5,6,4]))
         let expected = ListNode([7,0,8])
-        
-        XCTAssertNotNil(node)
-        XCTAssertNotNil(expected)
-        
         XCTAssertEqual(node?.val, expected?.val)
     }
     
     func test1() {
         let node = solution.addTwoNumbers(ListNode([0]), ListNode([0]))
         let expected = ListNode([0])
-        
-        XCTAssertNotNil(node)
-        XCTAssertNotNil(expected)
-        
         XCTAssertEqual(node?.val, expected?.val)
     }
     
     func test2() {
         let node = solution.addTwoNumbers(ListNode([9,9,9,9,9,9,9]), ListNode([9,9,9,9]))
         let expected = ListNode([8,9,9,9,0,0,0,1])
-        
-        XCTAssertNotNil(node)
-        XCTAssertNotNil(expected)
-        
         XCTAssertEqual(node?.val, expected?.val)
     }
 }
@@ -67,11 +54,10 @@ public class ListNode {
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
     
-    // An additional initializer that can be used to implement nodes from an array
     public init?(_ array: [Int]) {
         guard !array.isEmpty else { return nil }
         self.val = array[0]
-        var prev: ListNode = self
+        var prev = self
         for i in 1..<array.count {
             let new = ListNode(array[i])
             prev.next = new
