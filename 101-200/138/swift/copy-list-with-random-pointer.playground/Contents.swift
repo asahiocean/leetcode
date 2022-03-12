d26fd5c9a031ee37c8a4e7df8fd6c450438f48f8
asahiocean <61920574+asahiocean@users.noreply.github.com>
2022-03-12 07:24:24 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 101-200/138/swift/copy-list-with-random-pointer.playground/Contents.swift
+++ 101-200/138/swift/copy-list-with-random-pointer.playground/Contents.swift
@@ -7,32 +7,32 @@ class Solution {
     func copyRandomList(_ head: Node?) -> Node? {
         if head == nil { return nil }
         
-        var curr = head, idx = 0
+        var temp = head, idx = 0
         
-        while let node = curr {
-            node.val = (node.val + 10000) | (idx << 32)
-            curr = curr?.next
+        while let tmp = temp {
+            tmp.val = (tmp.val + 10000) | (idx << 32)
+            temp = tmp.next
             idx += 1
         }
         
         var nodes: [Node?] = []
-        curr = head
+        temp = head
         
-        while let node = curr {
-            let copy = Node((node.val & Int(UInt32.max)) - 10000)
-            if !nodes.isEmpty { nodes[nodes.count - 1]?.next = copy }
-            nodes.append(copy)
-            curr = curr?.next
+        while let tmp = temp {
+            let node = Node((tmp.val & Int(UInt32.max)) - 10000)
+            if !nodes.isEmpty { nodes[nodes.count - 1]?.next = node }
+            nodes.append(node)
+            temp = tmp.next
         }
         
-        curr = head
+        temp = head
         idx = 0
         
-        while let node = curr {
-            if let node = node.random {
-                nodes[idx]?.random = nodes[node.val >> 32]
+        while let tmp = temp {
+            if let rand = tmp.random {
+                nodes[idx]?.random = nodes[rand.val >> 32]
             }
-            curr = curr?.next
+            temp = temp?.next
             idx += 1
         }
         return nodes[0]
