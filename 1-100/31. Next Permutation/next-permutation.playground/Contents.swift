84d7a7cd8d1ec3577cbef1a25270b66d0ed30efc
asahiocean <asahiocean@icloud.com>
2022-03-29 23:45:09 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/31. Next Permutation/next-permutation.playground/Contents.swift
+++ 1-100/31. Next Permutation/next-permutation.playground/Contents.swift
@@ -5,52 +5,53 @@ import Foundation
 
 class Solution {
     func nextPermutation(_ nums: inout [Int]) {
-        var l = -1, i = nums.count - 2
-        while i >= 0 {
-            if nums[i] < nums[i+1] { l = i; break }
-            i -= 1
+        var lhs = -1, rhs = -1, idx = nums.count - 2
+        while idx >= 0 {
+            if nums[idx] < nums[idx + 1] { lhs = idx; break }
+            idx -= 1
         }
-        guard l != -1 else {
+        guard lhs != -1 else {
             nums = Array(nums.reversed())
             return
         }
-        var r = -1
-        i = nums.count - 1
-        while i > l {
-            if nums[i] > nums[l] { r = i; break }
-            i -= 1
+        
+        idx = nums.count - 1
+        while idx > lhs {
+            if nums[idx] > nums[lhs] { rhs = idx; break }
+            idx -= 1
         }
-        nums.swapAt(l, r)
-        let arr: [Int] = Array(nums[l+1...nums.count - 1])
-        nums.replaceSubrange(l + 1..<nums.count, with: Array(arr).reversed())
+        nums.swapAt(lhs, rhs)
+        let arr = [Int](nums[lhs+1...nums.count - 1])
+        nums.replaceSubrange(lhs + 1..<nums.count, with: Array(arr.reversed()))
     }
 }
 
+// MARK: - Test cases -
+
+// Result: Executed 4 tests, with 0 failures (0 unexpected) in 0.111 (0.113) seconds
+
 import XCTest
 
-// Executed 4 tests, with 0 failures (0 unexpected) in 0.111 (0.113) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
         var nums = [1,2,3]
-        s.nextPermutation(&nums)
+        solution.nextPermutation(&nums)
         XCTAssertEqual(nums, [1,3,2])
     }
+    
     func test1() {
         var nums = [3,2,1]
-        s.nextPermutation(&nums)
+        solution.nextPermutation(&nums)
         XCTAssertEqual(nums, [1,2,3])
     }
+    
     func test2() {
         var nums = [1,1,5]
-        s.nextPermutation(&nums)
+        solution.nextPermutation(&nums)
         XCTAssertEqual(nums, [1,5,1])
-    }
-    func test3() {
-        var nums = [1]
-        s.nextPermutation(&nums)
-        XCTAssertEqual(nums, [1])
     }
 }
 
