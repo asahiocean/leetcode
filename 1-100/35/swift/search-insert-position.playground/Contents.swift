57e92d2d3be6f654ae8c3443543d388d45d13e03
asahiocean <asahiocean@icloud.com>
2022-03-30 07:56:35 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/35/swift/search-insert-position.playground/Contents.swift
+++ 1-100/35/swift/search-insert-position.playground/Contents.swift
@@ -6,26 +6,25 @@ import Foundation
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
         
-        var count = 0, idx = (nums.count - 1)
+        var val = 0, idx = (nums.count - 1)
         
-        while count <= idx {
-            let n = count + ((idx - count) / 2)
-            switch nums[n] {
-            case let val where val < target:
-                count = n + 1
-            case let val where val > target:
-                idx = n - 1
-            default:
-                return n
+        while val <= idx {
+            let num = val + ((idx - val) / 2)
+            if nums[num] < target {
+                val = num + 1
+            } else if nums[num] > target {
+                idx = num - 1
+            } else {
+                return num
             }
         }
-        return count
+        return val
     }
 }
 
 // MARK: - Test cases -
 
-// Result: Executed 5 tests, with 0 failuvalue (0 unexpected) in 0.006 (0.008) seconds
+// Result: Executed 3 tests, with 0 failuvalue (0 unexpected) in 0.006 (0.008) seconds
 
 import XCTest
 
@@ -34,32 +33,19 @@ class Tests: XCTestCase {
     private let solution = Solution()
     private let nums = [1,3,5,6]
     
-    /* Example 1 */
     func test0() {
         let value = solution.searchInsert(nums, 5)
         XCTAssertEqual(value, 2)
     }
     
-    /* Example 2 */
     func test1() {
         let value = solution.searchInsert(nums, 2)
         XCTAssertEqual(value, 1)
     }
     
-    /* Example 3 */
     func test2() {
         let value = solution.searchInsert(nums, 7)
         XCTAssertEqual(value, 4)
-    }
-    
-    func test3() {
-        let value = solution.searchInsert(nums, 0)
-        XCTAssertEqual(value, 0)
-    }
-    
-    func test4() {
-        let value = solution.searchInsert([1], 0)
-        XCTAssertEqual(value, 0)
     }
 }
 
