31a2594adc841ded4e3aba791f03bf9964600ebb
asahiocean <asahiocean@icloud.com>
2022-03-30 08:25:32 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1-100/37. Sudoku Solver/sudoku-solver.playground/Contents.swift
+++ 1-100/37. Sudoku Solver/sudoku-solver.playground/Contents.swift
@@ -4,29 +4,21 @@ import Foundation
 // https://leetcode.com/problems/sudoku-solver/
 
 class Solution {
-    func solveSudoku(_ board: inout [[Character]]) {
+    internal typealias Char = Character
+    func solveSudoku(_ board: inout [[Char]]) {
         guard board.count != 0 || board[0].count != 0 else { return }
         helper(&board)
     }
     
-    private func helper(_ board: inout [[Character]]) -> Bool {
-        func isValid(_ i: Int, _ j: Int, _ num: Character) -> Bool {
-            let m = board.count, n = board[0].count
-            for x in 0..<n where board[i][x] == num { return false }    // row
-            for y in 0..<m where board[y][j] == num { return false }    // col
-            for x in i / 3 * 3..<i / 3 * 3 + 3 {                        // square
-                for y in j / 3 * 3..<j / 3 * 3 + 3 where board[x][y] == num { return false }
-            }
-            return true
-        }
-        for i in 0..<board.count {
-            for j in 0..<board[0].count where board[i][j] == "." {
-                for num in 1...9 where isValid(i, j, Character("\(num)")) {
-                    board[i][j] = Character("\(num)")
+    private func helper(_ board: inout [[Char]]) -> Bool {
+        for r in 0..<board.count {
+            for c in 0..<board[0].count where board[r][c] == "." {
+                for n in 1...9 where isValid(board, Char("\(n)"), r, c) {
+                    board[r][c] = Char("\(n)")
                     if helper(&board) {
                         return true
                     } else {
-                        board[i][j] = "."
+                        board[r][c] = "."
                     }
                 }
                 return false
@@ -34,14 +26,25 @@ class Solution {
         }
         return true
     }
+    private func isValid(_ board: [[Char]], _ ch: Char, _ r: Int,  _ c: Int) -> Bool {
+        for i in 0...8 {
+            if board[r][i] == ch || board[i][c] == ch { return false }
+            if board[(r / 3) * 3 + i / 3][(c / 3) * 3 + i % 3] == ch { return false }
+        }
+        return true
+    }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 1 test, with 0 failures (0 unexpected) in 0.265 (0.267) seconds
 
 import XCTest
 
-// Executed 1 test, with 0 failures (0 unexpected) in 0.498 (0.499) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test() {
         var arrChars: [[Character]] = [["5","3",".",".","7",".",".",".","."],
                                        ["6",".",".","1","9","5",".",".","."],
@@ -52,7 +55,7 @@ class Tests: XCTestCase {
                                        [".","6",".",".",".",".","2","8","."],
                                        [".",".",".","4","1","9",".",".","5"],
                                        [".",".",".",".","8",".",".","7","9"]]
-        s.solveSudoku(&arrChars)
+        solution.solveSudoku(&arrChars)
         XCTAssertEqual(arrChars, [["5","3","4","6","7","8","9","1","2"],
                                   ["6","7","2","1","9","5","3","4","8"],
                                   ["1","9","8","3","4","2","5","6","7"],
