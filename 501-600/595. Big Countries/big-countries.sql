326e3b883628a81ee95559e08933252ff1f724b1
asahiocean <asahiocean@icloud.com>
2022-04-08 01:42:28 +0500

    595-mysql-rm-old
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 501-600/595. Big Countries/big-countries.sql
+++
@@ -1,6 +0,0 @@
--- 595. Big Countries
--- https://leetcode.com/problems/big-countries/
-
-# Write your MySQL query statement below
-SELECT name, population, area FROM World
-WHERE population > 25000000 OR area > 3000000;
