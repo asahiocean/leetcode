2006eed2d1921272a320fde734ee03e82418aff1
asahiocean <asahiocean@icloud.com>
2022-04-13 18:38:14 +0500

    1729-mysql-solution
    1729. Find Followers Count
    https://leetcode.com/problems/find-followers-count/
    
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 1701-1800/1729/find-followers-count.sql
@@ -0,0 +1,5 @@
+-- 1729. Find Followers Count
+-- https://leetcode.com/problems/find-followers-count/
+
+SELECT USER_ID, COUNT(*) AS followers_count
+FROM Followers GROUP BY USER_ID ORDER BY USER_ID
