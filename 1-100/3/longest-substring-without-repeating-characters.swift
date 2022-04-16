f4113828ed1bc6ba6f986effe9414a1283bf9ba5
asahiocean <asahiocean@icloud.com>
2022-04-16 21:29:15 +0500

    3-swift-solution-new
    3. Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ 1-100/3/longest-substring-without-repeating-characters.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// 3. Longest Substring Without Repeating Characters
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        guard !s.isEmpty else { return 0 }
+        var len = 0, chars: [Character] = []
+        for c in s {
+            if let idx = chars.firstIndex(of: c) {
+                chars.removeSubrange(0...idx)
+            }
+            chars.append(c)
+            len = max(len, chars.count)
+        }
+        return len
+    }
+}
+
+// Playground and test cases: https://leetcode.com/playground/RCFf98E3
