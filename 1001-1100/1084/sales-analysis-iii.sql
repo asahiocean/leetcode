bd99ab0281274a67e0c479aea8150808a19eded3
asahiocean <asahiocean@icloud.com>
2022-04-16 10:00:18 +0500

    1084-mysql-solution
    1084. Sales Analysis III
    https://leetcode.com/problems/sales-analysis-iii/
    
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 1001-1100/1084/sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+-- 1084. Sales Analysis III
+-- https://leetcode.com/problems/sales-analysis-iii/
+
+SELECT product_id, product_name FROM product
+WHERE product_id NOT IN (
+    SELECT product_id FROM sales WHERE sale_date
+    NOT BETWEEN '2019-01-01' AND '2019-03-31'
+)
