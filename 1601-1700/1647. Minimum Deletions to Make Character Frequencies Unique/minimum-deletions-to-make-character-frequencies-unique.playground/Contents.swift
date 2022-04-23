0509b7db6f2f15a7e1c695ade7773d69a8e3703b
asahiocean <asahiocean@icloud.com>
2022-04-23 11:26:49 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1601-1700/1647. Minimum Deletions to Make Character Frequencies Unique/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
+++ 1601-1700/1647. Minimum Deletions to Make Character Frequencies Unique/minimum-deletions-to-make-character-frequencies-unique.playground/Contents.swift
@@ -5,44 +5,43 @@ import Foundation
 
 class Solution {
     func minDeletions(_ s: String) -> Int {
-        guard s.count > 0 else { return 0 }
-        
-        var count: Int = 0
-        
-        var dict: [Character:Int] = [:]
-        s.forEach({ dict[$0, default: 0] += 1 })
-        
-        var set: Set<Int> = []
-        dict.forEach({
-            guard var value = dict[$0.key] else { return }
-            while set.contains(value) {
-                value -= 1
-                count += 1
+        guard !s.isEmpty else { return 0 }
+        var res = 0, set = Set<Int>()
+        let dict = Dictionary(s.map({($0, 1)}), uniquingKeysWith: +)
+        dict.forEach {
+            guard var idx = dict[$0.key] else { return }
+            while set.contains(idx) {
+                idx -= 1
+                res += 1
             }
-            if value > 0 { set.insert(value) }
-        })
-        return count
+            if idx > 0 { set.insert(idx) }
+        }
+        return res
     }
 }
 
 // MARK: - Test Cases -
 
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.021 (0.023) seconds
+
 import XCTest
 
-// Executed 3 tests, with 0 failures (0 unexpected) in 0.031 (0.033) seconds
-
 class Tests: XCTestCase {
-    private let s = Solution()
+    
+    private let solution = Solution()
+    
     func test0() {
-        let res = s.minDeletions("aab")
+        let res = solution.minDeletions("aab")
         XCTAssertEqual(res, 0)
     }
+    
     func test1() {
-        let res = s.minDeletions("aaabbbcc")
+        let res = solution.minDeletions("aaabbbcc")
         XCTAssertEqual(res, 2)
     }
+    
     func test2() {
-        let res = s.minDeletions("ceabaacb")
+        let res = solution.minDeletions("ceabaacb")
         XCTAssertEqual(res, 2)
     }
 }
