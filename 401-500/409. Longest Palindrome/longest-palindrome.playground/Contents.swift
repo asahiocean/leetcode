f256fae51b5732955b71cf68ee98bedef5e35649
asahiocean <asahiocean@icloud.com>
2022-04-23 12:05:36 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 401-500/409. Longest Palindrome/longest-palindrome.playground/Contents.swift
+++ 401-500/409. Longest Palindrome/longest-palindrome.playground/Contents.swift
@@ -5,26 +5,23 @@ import Foundation
 
 class Solution {
     func longestPalindrome(_ s: String) -> Int {
-        
-        var visited: Set<Character> = []
-        var value = 0
-        
-        s.forEach { (ch: Character) in
-            if visited.contains(ch) {
-                value += 2
-                visited.remove(ch)
+        guard !s.isEmpty else { return 0 }
+        var res = 0, set = Set<Character>()
+        for ch in s {
+            if set.contains(ch) {
+                res += 2
+                set.remove(ch)
             } else {
-                visited.insert(ch)
+                set.insert(ch)
             }
         }
-        
-        return !(visited.isEmpty) ? value + 1 : value
+        return !set.isEmpty ? res + 1 : res
     }
 }
 
 // MARK: - Test cases -
 
-// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.009 (0.011) seconds
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
 
 import XCTest
 
@@ -32,7 +29,7 @@ class Tests: XCTestCase {
     
     private let solution = Solution()
     
-    /// One longest palindrome that can be built is "dccaccd", whose length is 7.
+    // One longest palindrome that can be built is "dccaccd", whose length is 7.
     func test0() {
         let value = solution.longestPalindrome("abccccdd")
         XCTAssertEqual(value, 7)
