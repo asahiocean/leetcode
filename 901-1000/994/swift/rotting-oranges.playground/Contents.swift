be5d9de29382df49c8557dd0c9ce8c4c8ff0c423
asahiocean <asahiocean@icloud.com>
2022-04-23 11:05:24 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 901-1000/994/swift/rotting-oranges.playground/Contents.swift
+++ 901-1000/994/swift/rotting-oranges.playground/Contents.swift
@@ -4,56 +4,41 @@ import Foundation
 // https://leetcode.com/problems/rotting-oranges/
 
 class Solution {
-    
-    private typealias Direction = (dx: Int, dy: Int)
-    private let dirs: [Direction] = [(0,1),(0,-1),(1,0),(-1,0)]
-    
-    func orangesRotting(_ grid: [[Int]]) -> Int {
-        
-        let rows = grid.count, cols = grid[0].count
-        
-        var queue: [Int] = [], oranges: Set<Int> = []
-        
-        for r in 0..<rows {
-            for c in 0..<cols {
-                switch grid[r][c] {
-                case 1: oranges.insert(r << 8 | c)
-                case 2: queue.append(r << 8 | c)
+    func orangesRotting(_ g: [[Int]]) -> Int {
+        var queue: [Int] = [], orng: Set<Int> = []
+        for r in 0..<g.count {
+            for c in 0..<g[0].count {
+                let val = r << 8 | c
+                switch g[r][c] {
+                case 1: orng.insert(val)
+                case 2: queue.append(val)
                 default: break
                 }
             }
         }
-        
         func check(_ x: Int, _ y: Int) -> Bool {
-            return x < rows && x >= 0 && y < cols && y >= 0
+            return x < g.count && x >= 0 && y < g[0].count && y >= 0
         }
-        
-        var neededTime = 0
+        var time = 0
         while !queue.isEmpty {
-            
-            var nextlvl: [Int] = []
-            
+            var lvl: [Int] = []
             for p in queue {
-                
-                let x = p >> 8
-                let y = p & 0xff
-                
-                for d in dirs {
-                    let nextX = x + d.dx
-                    let nextY = y + d.dy
-                    if check(nextX, nextY)  {
-                        let nextP = (nextX << 8 | nextY)
-                        if oranges.contains(nextP) {
-                            oranges.remove(nextP)
-                            nextlvl.append(nextP)
+                let x = p >> 8, y = p & 0xff
+                for (dx,dy) in [(0,1),(0,-1),(1,0),(-1,0)] {
+                    let next = (x: x + dx, y: y + dy)
+                    if check(next.x, next.y)  {
+                        let nextP = (next.x << 8 | next.y)
+                        if orng.contains(nextP) {
+                            orng.remove(nextP)
+                            lvl.append(nextP)
                         }
                     }
                 }
             }
-            queue = nextlvl
-            if !queue.isEmpty { neededTime += 1 }
+            queue = lvl
+            if !queue.isEmpty { time += 1 }
         }
-        return oranges.isEmpty ? neededTime : -1
+        return orng.isEmpty ? time : -1
     }
 }
 
