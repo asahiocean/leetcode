4db03eb54d1f7ceca6eaf0be3d50702e0890ad2a
asahiocean <asahiocean@icloud.com>
2022-04-23 09:53:35 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 501-600/567/swift/permutation-in-string.playground/Contents.swift
+++ 501-600/567/swift/permutation-in-string.playground/Contents.swift
@@ -5,31 +5,24 @@ import Foundation
 
 class Solution {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
-        let arrS2 = [Character](s2)
-        
-        var win: Dictionary<Character,Int> = [:]
-        var need: Dictionary<Character,Int> = [:]
-        
+        typealias DCI = [Character:Int]
+        let as2 = Array(s2)
+        var lhs = 0, rhs = 0, match = 0, need = DCI(), win = DCI()
         for ch in s1 { need[ch, default: 0] += 1 }
-        
-        var lhs = 0, rhs = 0, match = 0
-        
-        while rhs < arrS2.count {
-            let elr = arrS2[rhs]
+        while rhs < as2.count {
+            let a2r = as2[rhs]
             rhs += 1
-            
-            if need[elr] != nil {
-                win[elr, default: 0] += 1
-                if win[elr] == need[elr] { match += 1 }
+            if let n2r = need[a2r] {
+                win[a2r, default: 0] += 1
+                if win[a2r] == n2r { match += 1 }
             }
-            
             while match == need.count {
                 if rhs - lhs == s1.count { return true }
-                let itemL = arrS2[lhs]
+                let a2l = as2[lhs]
                 lhs += 1
-                if let ell = need[itemL] {
-                    if win[itemL] == ell { match -= 1 }
-                    win[itemL]! -= 1
+                if let n2l = need[a2l] {
+                    if win[a2l] == n2l { match -= 1 }
+                    win[a2l, default: 0] -= 1
                 }
             }
         }
