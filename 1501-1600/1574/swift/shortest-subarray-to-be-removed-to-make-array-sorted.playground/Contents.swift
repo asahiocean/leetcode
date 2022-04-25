dad5c45a02bdcb7ca22f9b92ac0b0d2f0b451e37
asahiocean <asahiocean@icloud.com>
2022-04-25 20:36:34 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 1501-1600/1574/swift/shortest-subarray-to-be-removed-to-make-array-sorted.playground/Contents.swift
+++ 1501-1600/1574/swift/shortest-subarray-to-be-removed-to-make-array-sorted.playground/Contents.swift
@@ -27,3 +27,37 @@ class Solution {
         return res
     }
 }
+
+// MARK: - Test cases -
+
+// Result: Executed 3 tests, with 0 failures (0 unexpected) in 0.008 (0.011) seconds
+
+import XCTest
+
+class Tests: XCTestCase {
+
+    private let solution = Solution()
+    
+    // The shortest subarray we can remove is [10,4,2] of length 3.
+    // The remaining elements after that will be [1,2,3,3,5] which are sorted.
+    // Another correct solution is to remove the subarray [3,10,4].
+    func test0() {
+        let value = solution.findLengthOfShortestSubarray([1,2,3,10,4,2,3,5])
+        XCTAssertEqual(value, 3)
+    }
+    
+    // Since the array is strictly decreasing, we can only keep a single element.
+    // Therefore we need to remove a subarray of length 4, either [5,4,3,2] or [4,3,2,1].
+    func test1() {
+        let value = solution.findLengthOfShortestSubarray([5,4,3,2,1])
+        XCTAssertEqual(value, 4)
+    }
+    
+    // The array is already non-decreasing. We do not need to remove any elements.
+    func test2() {
+        let value = solution.findLengthOfShortestSubarray([1,2,3])
+        XCTAssertEqual(value, 0)
+    }
+}
+
+Tests.defaultTestSuite.run()
