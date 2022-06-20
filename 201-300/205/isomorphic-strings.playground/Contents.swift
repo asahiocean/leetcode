cf35bc5bbb8ea5df86adc05a3de9fde48ed8c252
asahiocean <asahiocean@icloud.com>
2022-06-20 06:25:02 +0500

    205-swift-solution
    205. Isomorphic Strings
    https://leetcode.com/problems/isomorphic-strings/
    
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

---
+++ 201-300/205/isomorphic-strings.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/
+
+class Solution {
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        var sdc = [Character:String.Index](), tdc = sdc
+        for i in s.indices {
+            guard sdc[s[i]] == tdc[t[i]] else { return false }
+            sdc[s[i]] = i
+            tdc[t[i]] = i
+        }
+        return true
+    }
+}
