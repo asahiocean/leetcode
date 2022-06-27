20fb653c379efe8399a86afaf257092990571173
asahiocean <asahiocean@icloud.com>
2022-06-27 08:29:00 +0500

    Update Contents.swift
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- 101-200/200/swift/number-of-islands.playground/Contents.swift
+++ 101-200/200/swift/number-of-islands.playground/Contents.swift
@@ -4,24 +4,24 @@ import Foundation
 // https://leetcode.com/problems/number-of-islands/
 
 class Solution {
-    func numIslands(_ grid: [[Character]]) -> Int {
-        let rows = grid.count, cols = grid[0].count
-        guard rows > 0 && cols > 0 else { return 0 }
-        
-        var count = 0, gridCopy = grid
+    func numIslands(_ g: [[Character]]) -> Int {
+        guard !g.isEmpty else { return 0 }
+        let rows = g.count, cols = g[0].count
+        var val = 0, grid = g
+
+        func dfs(_ m: Int, _ n: Int, _ a: Int, _ b: Int) {
+            guard a >= 0 && a < m && b >= 0 && b < n && grid[a][b] != "0" else { return }
+            grid[a][b] = "0"
+            for (a,b) in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)] { dfs(m, n, a, b) }
+        }
         
         for a in 0..<rows {
-            for b in 0..<cols where gridCopy[a][b] == "1" {
-                dfs(&gridCopy, rows, cols, a, b)
-                count += 1
+            for b in 0..<cols where grid[a][b] == "1" {
+                dfs(rows, cols, a, b)
+                val += 1
             }
         }
-        return count
-    }
-    private func dfs(_ grid: inout [[Character]], _ m: Int, _ n: Int, _ a: Int, _ b: Int) {
-        guard a >= 0 && a < m && b >= 0 && b < n && grid[a][b] != "0" else { return }
-        grid[a][b] = "0"
-        for (a,b) in [(a+1, b), (a-1, b), (a, b+1), (a, b-1)] { dfs(&grid, m, n, a, b) }
+        return val
     }
 }
 
